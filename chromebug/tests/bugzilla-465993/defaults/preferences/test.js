4357d6326399a3e25f1ed8a6f2108e0464772c22
johnjbarton.com <johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2008-11-25 17:48:33 +0000

    Workaround 465998; open test window onload or setTimeout
    git-svn-id: http://fbug.googlecode.com/svn@1427 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- chromebug/tests/bugzilla-465993/defaults/preferences/test.js
+++ chromebug/tests/bugzilla-465993/defaults/preferences/test.js
@@ -5,12 +5,12 @@ user_pref("browser.dom.window.dump.enabled", true);
 user_pref("signed.applets.codebase_principal_support", true);
 
 // Allow localhost to execute privileged scripts
-user_pref("capability.principal.codebase.p1.granted", "UniversalXPConnect UniversalBrowserRead UniversalBrowserWrite UniversalPreferencesRead UniversalPreferencesWrite UniversalFileRead");
-user_pref("capability.principal.codebase.p1.id", "http://localhost:8888");
-user_pref("capability.principal.codebase.p1.subjectName", "");
+//user_pref("capability.principal.codebase.p1.granted", "UniversalXPConnect UniversalBrowserRead UniversalBrowserWrite UniversalPreferencesRead UniversalPreferencesWrite UniversalFileRead");
+//user_pref("capability.principal.codebase.p1.id", "http://localhost:8888");
+//user_pref("capability.principal.codebase.p1.subjectName", "");
 
 //Turn off default browser check
 user_pref("browser.shell.checkDefaultBrowser", false);
 
-user_pref("capability.principal.jdstest.id", "http://enki-pcshih.almaden.ibm.com:63636/ProjectExplorer/firebug/chromebug/tests/jsdTests@johnjbarton.com/content/events/listeners.html");
-user_pref("capability.principal.jsdtest.granted", "UniversalXPConnect");
+//user_pref("capability.principal.jdstest.id", "http://enki-pcshih.almaden.ibm.com:63636/ProjectExplorer/firebug/chromebug/tests/jsdTests@johnjbarton.com/content/events/listeners.html");
+//user_pref("capability.principal.jsdtest.granted", "UniversalXPConnect");
