3541ab24bd113bb2207b8002b26ca5db6747408d
odvarko@gmail.com <odvarko@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-02-12 13:28:04 +0000

    Modify example test so, there are no errors in the test suite.
    git-svn-id: http://fbug.googlecode.com/svn@1928 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- fbtest/branches/fbtest1.4/content/tests/examples/exampleTest.js
+++ fbtest/branches/fbtest1.4/content/tests/examples/exampleTest.js
@@ -2,8 +2,8 @@ function runTest()
 {
     FBTest.sysout("exampleTest.started");
     FBTest.ok(true, "This is a positive test");
-    FBTest.ok(false, "This is a negative test");
+    //FBTest.ok(false, "This is a negative test");
     FBTest.compare("Expected", "Expected", "Compare test (positive)");
-    FBTest.compare("Expected", "Actual", "Compare test (negative)");
+    //FBTest.compare("Expected", "Actual", "Compare test (negative)");
     FBTest.testDone();
 }
