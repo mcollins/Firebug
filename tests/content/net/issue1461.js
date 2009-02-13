033367d6f108aa544afc8fba31bac9247bbc744f
odvarko@gmail.com <odvarko@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-02-13 14:01:25 +0000

    git-svn-id: http://fbug.googlecode.com/svn@1954 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/net/issue1461.js
@@ -0,0 +1,43 @@
+// Test entry point.
+function runTest()
+{
+    FBTest.sysout("issue1461.START");
+    FBTest.loadScript("net/env.js", this);
+
+    openURL(basePath + "net/issue1461.html", function(win)
+    {
+        FBTest.sysout("issue1461.openNewTab; " + win.location.href);
+
+        var browser = FBTest.FirebugWindow;
+
+        // Open Firebug UI and activate Net panel.
+        browser.Firebug.showBar(true);
+        browser.FirebugChrome.selectPanel("net");
+
+        var panelNode = browser.FirebugContext.getPanel("net").panelNode;
+        expandNetRows(panelNode, "netRow", "category-html", "hasHeaders", "loaded");
+        expandNetTabs(panelNode, "netInfoResponseTab");
+
+        var responseBody = browser.FBL.getElementByClass(panelNode, "netInfoResponseText", 
+            "netInfoText");
+
+        // The response must be displayed.
+        FBTest.ok(responseBody, "Response tab must exist.");
+        if (!responseBody)
+            return testDone(win);
+
+        var partOfThePageSource = "<h1>Test for Issue #1461</h1>";
+        var index = responseBody.textContent.indexOf(partOfThePageSource);
+        FBTest.ok(index != -1, "The proper response is there.");
+
+        testDone(win);
+    });
+}
+
+function testDone(win)
+{
+    // Finish test
+    //removeCurrentTab();
+    FBTest.sysout("issue1461.DONE");
+    FBTest.testDone();
+}
