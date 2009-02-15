fd74aaf5caee27e4d8481f56aa0bc7d90f47672d
odvarko@gmail.com <odvarko@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-02-15 16:42:39 +0000

    New test for issue 1456
    git-svn-id: http://fbug.googlecode.com/svn@1969 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/net/1456/issue1456.js
@@ -0,0 +1,49 @@
+function runTest()
+{
+    FBTest.loadScript("net/env.js", this);
+    FBTest.sysout("issue1456.START");
+
+    var responseText = "$('tb').shake();\n$('tb').value='Some Response';\n";
+
+    // Server side handler.
+    FBTest.registerPathHandler("/net/1456/issue1456.txt", function (metadata, response)
+    {
+        FBTest.sysout("issue1456.onPathHandler; Server side handler executed.", metadata);
+
+        response.setHeader("Pragma", "no-cache", false);
+        response.setHeader("Cache-Control", "no-store, no-cache, max-age=0, must-revalidate", false);
+        response.setHeader("Content-type", "text/javascript", false);
+        response.write(responseText);
+    });
+
+    openNewTab(basePath + "net/1456/issue1456.htm", function(win)
+    {
+        // Open Firebug UI and activate Net panel.
+        FW.Firebug.showBar(true);
+        FW.FirebugChrome.selectPanel("net");
+
+        win.wrappedJSObject.runTest(function(response)
+        {
+            FBTest.sysout("issue1456.onResponse: ", response);
+
+            // Expand the test request with params
+            var panelNode = FW.FirebugContext.getPanel("net").panelNode;
+            FBTest.sysout("fbtest.panelNode", panelNode);
+            expandNetRows(panelNode, "netRow", "category-xhr", "hasHeaders", "loaded");
+            expandNetTabs(panelNode, "netInfoResponseTab");
+
+            // The response must be displayed.
+            var responseBody = FW.FBL.getElementByClass(panelNode, "netInfoResponseText", 
+                "netInfoText");
+
+            FBTest.ok(responseBody, "Response tab must exist.");
+            if (responseBody)
+                FBTest.compare(responseText, responseBody.textContent, "Response must match.");
+
+            // Finish test
+            //removeCurrentTab();
+            FBTest.sysout("issue1456.DONE");
+            FBTest.testDone();
+        })
+    })
+}
