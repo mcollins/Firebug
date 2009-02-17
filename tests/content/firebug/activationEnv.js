15f5bf01b6e02721b35fb0ddbca2b21c9d542134
johnjbarton.com <johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-02-17 06:30:39 +0000

    activation.js -> openOpenCloseClose, add openNotOpenClose
    git-svn-id: http://fbug.googlecode.com/svn@1999 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/firebug/activationEnv.js
+++ tests/content/firebug/activationEnv.js
@@ -1,9 +1,6 @@
 
-
 function initialize()
 {
-
-
     // ****************************************************************
     // Operations on Firebug
     FBTest.Firebug = {};
@@ -53,7 +50,7 @@ function initialize()
     FBTest.Firebug.TestHandlers = function(testName)
     {
         this.testName = testName;
-        this.progressElement = document.getElementById(testName);
+        this.progressElement = document.getElementById("firebugTestElement");
         if (!this.progressElement)
             throw new Error("TestHanders object requires element "+testName+" in document "+document.title);
     };
@@ -137,93 +134,4 @@ function initialize()
             FBTest.testDone();
         }
     };
-}
-
-
-// ------------------------------------------------------------------------
-// Individual sub tests
-var testNumber = 0;
-function openOpenCloseClose()
-{
-    var openOpenCloseCloseURL = "http://localhost:7080/firebug/OpenFirebugOnThisPage.html";
-
-
-    var openOpenCloseClose = new FBTest.Firebug.TestHandlers("openOpenCloseClose");
-
-    // Actual test operations
-    openOpenCloseClose.add( function onNewPage(event)
-    {
-        FBTrace.sysout("onNewPage starts", event);
-        var isFirebugOpen = FBTest.Firebug.isFirebugOpen();
-        FBTest.ok(!isFirebugOpen, "Firebug starts closed");
-
-        this.next = "onShowUI";
-        FBTest.Firebug.pressToggleFirebug();
-
-    });
-
-    openOpenCloseClose.add( function onShowUI()
-    {
-        var isFirebugOpen = FBTest.Firebug.isFirebugOpen();
-        FBTest.ok(isFirebugOpen, "Firebug now open");
-
-        FBTest.ok(this.next == "onShowUI", "showUI followed toggleFirebug");
-
-        if (FBTest.FirebugWindow.FirebugContext)
-        {
-            var contextName = FBTest.FirebugWindow.FirebugContext.getName();
-            FBTest.ok(true, "chromeWindow.FirebugContext "+contextName);
-            FBTest.ok(contextName == openOpenCloseCloseURL, "FirebugContext set to "+openOpenCloseCloseURL);
-        }
-        else
-            FBTest.ok(false, "no FirebugContext");
-        // now close it
-        this.next = "onHideUI";
-        FBTest.Firebug.pressToggleFirebug();
-
-    });
-
-    openOpenCloseClose.add( function onHideUI()
-    {
-        var isFirebugOpen = FBTest.Firebug.isFirebugOpen();
-        FBTest.ok(!isFirebugOpen, "Firebug now closed");
-
-        FBTest.ok(this.next == "onHideUI", "hideUI followed toggleFirebug");
-        openOpenCloseClose.done();
-    });
-
-    var uiListener =
-    {
-            showUI: function(browser, context) // called when the Firebug UI comes up in browser or detached
-            {
-                openOpenCloseClose.fire("onShowUI");
-            },
-
-            hideUI: function(brower, context)  // called when the Firebug UI comes down
-            {
-                openOpenCloseClose.fire("onHideUI");
-            },
-    };
-
-    // Now start the test.
-    FBTest.Firebug.setToKnownState();
-    openOpenCloseClose.fireOnNewPage("onNewPage", openOpenCloseCloseURL, uiListener);
-}
-
-//------------------------------------------------------------------------
-// Auto-run test
-
-function runTest()
-{
-    FBTest.sysout("Activation.started");
-    initialize();
-    FBTrace.sysout("activation.js FBTest", FBTest);
-
-    if (FBTest.FirebugWindow)
-        FBTest.ok(true, "We have the Firebug Window: "+FBTest.FirebugWindow.location);
-    else
-        FBTest.ok(false, "No Firebug Window");
-
-    // Auto run sequence
-    openOpenCloseClose();
 }
---
+++ tests/content/firebug/openOpenCloseClose.js
@@ -0,0 +1,120 @@
+
+
+
+function openOpenCloseClose()
+{
+    var openOpenCloseCloseURL = FBTest.getHTTPURLBase()+"firebug/OpenFirebugOnThisPage.html";
+
+    var openOpenCloseClose = new FBTest.Firebug.TestHandlers("openOpenCloseClose");
+
+    // Actual test operations
+    openOpenCloseClose.add( function onNewPage(event)
+    {
+        FBTrace.sysout("onNewPage starts", event);
+        var isFirebugOpen = FBTest.Firebug.isFirebugOpen();
+        FBTest.ok(!isFirebugOpen, "Firebug starts closed");
+
+        this.next = "onShowUI";
+        FBTest.Firebug.pressToggleFirebug();
+
+    });
+
+    openOpenCloseClose.add( function onShowUI()
+    {
+        var isFirebugOpen = FBTest.Firebug.isFirebugOpen();
+        FBTest.ok(isFirebugOpen, "Firebug now open");
+
+        FBTest.ok(this.next == "onShowUI", "showUI followed toggleFirebug");
+
+        if (FBTest.FirebugWindow.FirebugContext)
+        {
+            var contextName = FBTest.FirebugWindow.FirebugContext.getName();
+            FBTest.ok(true, "chromeWindow.FirebugContext "+contextName);
+            FBTest.ok(contextName == openOpenCloseCloseURL, "FirebugContext set to "+openOpenCloseCloseURL);
+        }
+        else
+            FBTest.ok(false, "no FirebugContext");
+        // now close it
+        this.next = "onHideUI";
+        FBTest.Firebug.pressToggleFirebug();
+
+    });
+
+    openOpenCloseClose.add( function onHideUI()
+    {
+        var isFirebugOpen = FBTest.Firebug.isFirebugOpen();
+        FBTest.ok(!isFirebugOpen, "Firebug now closed");
+
+        FBTest.ok(this.next == "onHideUI", "hideUI followed toggleFirebug");
+        openOpenCloseClose.done();
+    });
+
+    var uiListener =
+    {
+            showUI: function(browser, context) // called when the Firebug UI comes up in browser or detached
+            {
+                openOpenCloseClose.fire("onShowUI");
+            },
+
+            hideUI: function(brower, context)  // called when the Firebug UI comes down
+            {
+                openOpenCloseClose.fire("onHideUI");
+            },
+    };
+
+    // Now start the test.
+    FBTest.Firebug.setToKnownState();
+    openOpenCloseClose.fireOnNewPage("onNewPage", openOpenCloseCloseURL, uiListener);
+}
+
+function openNotOpenClose() 
+{
+    var openNotOpenCloseURL = FBTest.getHTTPURLBase()+"firebug/NeverOpenFirebugOnThisPage.html";
+
+
+    var openNotOpenClose = new FBTest.Firebug.TestHandlers("openNotOpenClose");
+
+    // Actual test operations
+    openNotOpenClose.add( function onNewPage(event)
+    {
+        FBTrace.sysout("onNewPage starts", event);
+        this.wasFirebugOpen = FBTest.Firebug.isFirebugOpen();
+        this.next = "onHideUI";
+    });
+    
+    var uiListener =
+    {
+            showUI: function(browser, context) // called when the Firebug UI comes up in browser or detached
+            {
+                FBTest.ok(false, "showUI should not be called on this page");
+            },
+
+            hideUI: function(broswer, context)  // called when the Firebug UI comes down
+            {
+                FBTest.ok(openNotOpenClose.wasFirebugOpen, "hideUI can be called only if Firebug was open");
+            },
+            showContext: function(browser, context)
+            {
+            	FBTest.ok(!context, "showContext should be called with null context");
+            },
+    };
+    openNotOpenClose.fireOnNewPage("onNewPage", openNotOpenCloseURL, uiListener);
+}
+
+//------------------------------------------------------------------------
+// Auto-run test
+
+function runTest()
+{
+    FBTest.sysout("Activation.started");
+    initialize();
+    FBTrace.sysout("activation.js FBTest", FBTest);
+
+    if (FBTest.FirebugWindow)
+        FBTest.ok(true, "We have the Firebug Window: "+FBTest.FirebugWindow.location);
+    else
+        FBTest.ok(false, "No Firebug Window");
+
+    // Auto run sequence
+    openOpenCloseClose();
+}
