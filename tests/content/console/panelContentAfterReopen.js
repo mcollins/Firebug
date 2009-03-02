af13200ea4ce8e78e006569912b399410e100cb6
odvarko@gmail.com <odvarko@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-03-02 17:11:15 +0000

    New test case: Console panel content must be preserved if Firebug is closed and opened.
    git-svn-id: http://fbug.googlecode.com/svn@2107 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/console/panelContentAfterReopen.js
@@ -0,0 +1,30 @@
+/**
+ * 1) Open a new tab and Firebug on it.
+ * 2) Select Enable Console panel.
+ * 3) Print simple log into the Console panel (test-page.runTest)
+ * 4) Close and open Firebug UI.
+ * 5) Verify that the Console panel log is still there.
+ */
+function runTest()
+{
+    FBTest.sysout("panelContentAfterReopen.START");
+    FBTestFirebug.openNewTab(basePath + "console/panelContentAfterReopen.html", function(win)
+    {
+        // Open Firebug UI and enable console panel.
+        FBTestFirebug.enableConsolePanel(function()
+        {
+            var panelNode = FBTestFirebug.selectPanel("console").panelNode;
+
+            FBTest.focus(FW.document.getElementById("fbCommandLine"));
+            win.wrappedJSObject.runTest();
+
+            FBTestFirebug.closeFirebug();
+            FBTestFirebug.openFirebug();
+
+            var logRow = FW.FBL.getElementByClass(panelNode, "logRow logRow-log");
+            var node = FW.FBL.getElementByClass(panelNode, "objectBox objectBox-text");
+            FBTest.compare("Hello From Test!", node.textContent, "The log must be still there.");
+            //FBTestFirebug.testDone("panelContentAfterReopen.DONE");
+        });
+    });
+}
