0946a189cdc44538fef74ef9bdaddf1668727dbb
johnjbarton.com <johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-03-03 01:41:29 +0000

    add test case for switching tabs
    git-svn-id: http://fbug.googlecode.com/svn@2109 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/firebug/closeOpenOpenSwitchTabsTwice.js
@@ -0,0 +1,50 @@
+// Test entry point.
+function runTest()
+{
+    FBTest.sysout("closeOpenOpenSwitchTabsTwice.START");
+    
+    FBTestFirebug.enableAllPanels();
+    FBTestFirebug.closeFirebugOnAllTabs(); // use the existing tab as the "no Firebug tab"
+    
+    var tabbrowser = FW.getBrowser();
+    var noFirebugTab = tabbrowser.selectedTab;
+    
+    FBTestFirebug.openNewTab(basePath + "firebug/OpenFirebugOnThisPage.html", function(win)
+    {
+        FBTest.progress("Open Firebug UI in this new tab");
+        FBTestFirebug.openFirebug();
+        
+        var theFirebuggedTab = tabbrowser.selectedTab;
+        
+        FBTest.progress("Switch back to the first tab.");
+        tabbrowser.selectedTab = noFirebugTab;
+        
+        FBTest.ok(!FBTestFirebug.isFirebugOpen(), "Firebug UI must be closed.");
+
+        checkIconOff('console');
+        checkIconOff('script');
+        checkIconOff('net');
+        
+        FBTest.progress("Switch again, to the Firebugged tab");
+        
+        tabbrowser.selectedTab = theFirebuggedTab;
+        FBTest.ok(FBTestFirebug.isFirebugOpen(), "Firebug UI must be opened now.");
+
+        checkIconOn('console');
+        checkIconOn('script');
+        checkIconOn('net');
+        
+        FBTestFirebug.testDone("closeOpenOpenSwitchTabsTwice.DONE");
+    });
+}
+
+function checkIconOff(panelName)
+{
+	var icon = FW.document.getElementById('fbStatusIcon').getAttribute(panelName);
+	FBTest.ok(!icon || (icon != "on"), "The "+panelName+" should NOT be marked on the Firebug Statusbar Icon");
+}
+
+function checkIconOn(panelName)
+{
+	var icon = FW.document.getElementById('fbStatusIcon').getAttribute(panelName);
+	FBTest.compare(icon+"", "on", "The "+panelName+" should be marked on the Firebug Statusbar Icon");}
