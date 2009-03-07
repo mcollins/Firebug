cc690d966c4175890397a320a94bf7bbb2dec9b8
odvarko@gmail.com <odvarko@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-03-07 08:58:47 +0000

    Fix for net panel activation test.
    git-svn-id: http://fbug.googlecode.com/svn@2184 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/net/activation/activation.js
+++ tests/content/net/activation/activation.js
@@ -1,38 +1,39 @@
 /**
- * 1) Open two new tabs and firebu on it.
- * 2) Disable net panel.
- * 3) Enable net panel.
- * 4) Select net panel. 
- * 5) Perform net request on the first tab and check net panel content.
- * 6) Perform net request on the second tab and check net panel content.
+ * 1) Open two new tabs and firebug on it with Net panel selected.
+ * 2) Disable and Enable net panel.
+ * 3) Perform net request on the first tab and check net panel content.
+ * 4) Perform net request on the second tab and check net panel content.
  */
 
 var tab1 = null;
 var tab2 = null;
-var counter = 0;
 
 function runTest()
 {
-    FBTest.sysout("panelContentAfterReopen.START");
-    tab1 = FBTestFirebug.openNewTab(basePath + "net/activation/activation1.html", onTabLoad);
-    tab2 = FBTestFirebug.openNewTab(basePath + "net/activation/activation2.html", onTabLoad);
+    FBTest.sysout("activation.START");
+
+    // Open two tabs one after another, open Firebug on both and select Net panel.
+    tab1 = FBTestFirebug.openNewTab(basePath + "net/activation/activation1.html", function() {
+        FBTestFirebug.openFirebug();
+        FW.FirebugChrome.selectPanel("net");
+        tab2 = FBTestFirebug.openNewTab(basePath + "net/activation/activation2.html", function() {
+            FBTestFirebug.openFirebug();
+            FW.FirebugChrome.selectPanel("net");
+            onRunTest();
+        });
+    });
 }
 
-function onTabLoad(window)
+function onRunTest(window)
 {
-    // Wait till both tabs are opened.
-    if (++counter < 2)
-        return;
-
-    // Select net panel and enable/disable.
-    var panel = FW.FirebugChrome.selectPanel("net");
+    // Disable and enable
     FBTestFirebug.disableNetPanel();
     FBTestFirebug.enableNetPanel();
 
     // Select first tab, execute XHR and verify. Once it's done do the same for the other tab. 
     selectTabAndVerify(tab1, function() {
         selectTabAndVerify(tab2, function() {
-            FBTestFirebug.testDone("panelContentAfterReopen.DONE");
+            FBTestFirebug.testDone("activation.DONE");
         });
     });
 }
@@ -42,17 +43,12 @@ function selectTabAndVerify(tab, callback)
     var tabbrowser = FW.getBrowser();
     tabbrowser.selectedTab = tab;
 
-    FBTestFirebug.openFirebug();
-
     var win = tab.linkedBrowser.contentWindow;
     win.wrappedJSObject.runTest(function(request)
     {
-        FBTest.sysout("activation.onResponse; " + request.channel.name, request);
-
         var panel = FW.FirebugChrome.selectPanel("net");
         var netRow = FW.FBL.getElementByClass(panel.panelNode, "netRow", "category-xhr", "hasHeaders", "loaded");
         FBTest.ok(netRow, "There must be one xhr request.");
-
         callback();
     });
 }
