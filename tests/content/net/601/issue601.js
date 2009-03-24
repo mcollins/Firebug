a11603b468445b9c0f061c3811e57a566b32b121
odvarko@gmail.com <odvarko@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-03-24 16:24:20 +0000

    Test for issue 601 (fireunit adapted for FBTest)
    git-svn-id: http://fbug.googlecode.com/svn@2279 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/net/601/issue601.js
@@ -0,0 +1,42 @@
+function runTest()
+{
+    FBTest.sysout("issue601.START");
+
+    // must be set to false in this test, but the original value is reverted.
+    var prefOrigValue = FBTestFirebug.getPref("showXMLHttpRequests");
+    FBTestFirebug.setPref("showXMLHttpRequests", false);
+
+    // Server side handler.
+    FBTest.registerPathHandler("/net/601/issue601.php", function (metadata, response)
+    {
+        response.setHeader("Content-type", "text/plain", false);
+        var postData = FW.FBL.readFromStream(metadata.bodyInputStream);
+        response.write(postData);
+    });
+
+    FBTestFirebug.openNewTab(basePath + "net/601/issue601.html", function(win)
+    {
+        FBTestFirebug.selectPanel("net");
+        FBTestFirebug.enableNetPanel(function(win)
+        {
+            var date = (new Date()).toUTCString();
+            var postData = "date=" + date;
+
+            win.wrappedJSObject.postRequest(postData, function(request)
+            {
+                // Expand Net's panel UI so, it's populated with data.
+                var panelNode = FBTestFirebug.getPanel("net").panelNode;
+                FBTestFirebug.expandElements(panelNode, "netRow", "category-xhr");
+                FBTestFirebug.expandElements(panelNode, "netInfoResponseTab");
+
+                var responseBody = FW.FBL.getElementByClass(panelNode, "netInfoResponseText", "netInfoText");
+                FBTest.ok(responseBody, "Response tab must exist.");
+                if (responseBody)
+                    FBTest.compare(postData, responseBody.textContent, "Test response must match.");
+
+                FBTestFirebug.setPref("showXMLHttpRequests", prefOrigValue);
+                FBTestFirebug.testDone("issue601.DONE");
+            });
+        });
+    });
+}
