c51e8b8c5fe0438200fc36da0c0c31eca760b991
johnjbarton.com <johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-03-30 22:21:39 +0000

    simplified test based on UI event
    git-svn-id: http://fbug.googlecode.com/svn@2335 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/firebug/openNotOpenClose.js
+++ tests/content/firebug/openNotOpenClose.js
@@ -13,31 +13,8 @@ function openNotOpenClose()
         openNotOpenClose.done();
     });
 
-    var testListener =
-    {
-        uiListener:
-        {
-            showUI: function(browser, context) // called when the Firebug UI comes up in browser or detached
-            {
-                FBTest.ok(false, "showUI should not be called on this page");
-            },
-
-            hideUI: function(broswer, context)  // called when the Firebug UI comes down
-            {
-                FBTest.ok(openNotOpenClose.wasFirebugOpen, "hideUI can be called only if Firebug was open");
-            },
-        },
-        moduleListener:
-        {
-            showContext: function(browser, context)
-            {
-                    FBTest.ok( !(context), "showContext should be called with null context");
-                    FBTest.sysout("openNotOpenClose showContext "+(!context), context);
-            },
-        }
-    };
     openNotOpenClose.wasFirebugOpen = FBTest.Firebug.isFirebugOpen();
-    openNotOpenClose.fireOnNewPage("onNewPage", openNotOpenCloseURL, testListener);
+    openNotOpenClose.fireOnNewPage("onNewPage", openNotOpenCloseURL);
 }
 
 //------------------------------------------------------------------------
