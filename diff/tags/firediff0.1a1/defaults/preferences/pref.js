bb0ec8500495a68167ac4afcaba93341392d89a5
kpdecker <kpdecker@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-04-20 06:56:00 +0000

    Tag 0.1a1 release version (late is better than never)
    git-svn-id: http://fbug.googlecode.com/svn@2585 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ diff/tags/firediff0.1a1/defaults/preferences/pref.js
@@ -0,0 +1,6 @@
+pref("extensions.firebug.firediff.enableSites", false);
+pref("extensions.firebug.firediff.displayAppChanges", true);
+pref("extensions.firebug.firediff.displayFirebugChanges", true);
+
+//Support for tracing console
+pref("extensions.firebug.DBG_FIREDIFF", false);
