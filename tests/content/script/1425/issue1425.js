c82fafd24723f29d31b9d4abf7c61c06230844e4
johnjbarton.com <johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-04-20 20:07:41 +0000

    convert to closure based test case
    git-svn-id: http://fbug.googlecode.com/svn@2593 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/script/1425/issue1425.js
+++ tests/content/script/1425/issue1425.js
@@ -8,29 +8,22 @@ function runTest()
     var issue1425URL = basePath + "script/1425/issue1425.html";
     var issue1425 = new FBTest.Firebug.TestHandlers("issue1425");
 
-    issue1425.add( function onNewPage(event)
+    FBTestFirebug.openNewTab(issue1425URL, function(win)
     {
         var isOpen = FBTest.Firebug.isFirebugOpen();
-        FBTest.sysout("onNewPage starts with isFirebugOpen:"+isOpen, event);
+        FBTest.sysout("onNewPage starts with isFirebugOpen:"+isOpen+" in "+win.location);
         if (!isOpen)
             FBTest.Firebug.pressToggleFirebug();
-    });
 
-
-    issue1425.add( function navigateToSource(event)
-    {
             var panel = FW.FirebugChrome.selectPanel("script");
 
-            issue1425.userHasNavigated = true;
+            FBTest.progress("Navigated to script panel");
 
             // Select proper JS file.
             var found = FBTestFirebug.selectPanelLocationByName(panel, "main.js");
             FBTest.compare(found, true, "The main.js should be found");
 
-    });
 
-    issue1425.add( function checkSource(event)
-    {
         var panel = FW.FirebugContext.chrome.getSelectedPanel();
         var selectedLocationDescription = panel.getObjectDescription(panel.location);
         FBTest.compare("main.js", selectedLocationDescription.name,  "The selected location must be main.js");
@@ -49,78 +42,35 @@ function runTest()
                     "Verify source on line 1");
 
             // Scroll to 1143
-            issue1425.userHasScrolled = true;
+            FBTest.progress("Scroll to line 1143")
             FBTest.Firebug.selectSourceLine(panel.location.href, 1143, "js");
+            setTimeout( function checkScrolling()
+            {
+                    FBTest.progress("check scrolling");
+                    // Look for line 1143
+                    var row1143 = FBTestFirebug.getSourceLineNode(1143);
+
+                    // Check 1143
+                    FBTest.ok(row1143, "The row 1143 must exist");
+                    if (row1143)
+                    {
+                        var source1143 = "initialize:function(config){";
+                        FBTest.compare(source1143, row1143.firstChild.nextSibling.textContent,
+                            "The source code at line 1143 verified.");
+                    }
+                    else
+                    {
+
+                        FBTest.sysout("Where is 1143 row in "+panel.location.href, rows);
+                    }
+
+                    issue1425.done();
+            });
         }
         else
         {
             issue1425.done();
         }
     });
-
-    issue1425.add( function checkScrolling()
-    {
-            FBTest.progress("check scrolling");
-            // Look for line 1143
-            var row1143 = FBTestFirebug.getSourceLineNode(1143);
-
-            // Check 1143
-            FBTest.ok(row1143, "The row 1143 must exist");
-            if (row1143)
-            {
-                var source1143 = "initialize:function(config){";
-                FBTest.compare(source1143, row1143.firstChild.nextSibling.textContent,
-                    "The source code at line 1143 verified.");
-            }
-            else
-            {
-
-                FBTest.sysout("Where is 1143 row in "+panel.location.href, rows);
-            }
-
-            issue1425.done();
-    });
-
-
-    var testListener =
-    {
-            moduleListener:
-            {
-                loadedContext: function(context)
-                {
-                    if (issue1425.userIsActive)
-                        issue1425.fire("navigateToSource");
-                },
-                destroyContext: function(context)
-                {
-
-                }
-            },
-            uiListener:
-            {
-                onPanelNavigate: function()
-                {
-                    if (issue1425.userIsActive && issue1425.userHasNavigated && !issue1425.userHasScrolled)
-                        issue1425.fire("checkSource");
-                },
-                showUI: function(browser, context)
-                {
-                    issue1425.userIsActive = true;
-                    FBTest.progress("showUI userIsActive "+issue1425.userIsActive+" context.loaded "+context.loaded);
-                    if (context.loaded)
-                        issue1425.fire("navigateToSource");
-                },
-                onViewportChange: function()
-                {
-                    if (issue1425.userHasScrolled)
-                    {
-                        FBTest.progress("onViewportChange");
-                        issue1425.fire("checkScrolling");
-                    }
-                }
-            }
-    };
-    issue1425.userIsActive = false;
-    issue1425.fireOnNewPage("onNewPage", issue1425URL,  testListener);
 }
 
