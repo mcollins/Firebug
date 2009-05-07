bd6fe53d0465eba6763775b8a1dbc82aaae76509
johnjbarton.com <johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-05-07 00:36:11 +0000

    better progress messages
    git-svn-id: http://fbug.googlecode.com/svn@2776 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/console/panelContentAfterReopen.js
+++ tests/content/console/panelContentAfterReopen.js
@@ -18,12 +18,14 @@ function runTest()
             FBTest.focus(FW.document.getElementById("fbCommandLine"));
             win.wrappedJSObject.runTest();
 
+            FBTest.progress("close Firebug");
             FBTestFirebug.closeFirebug();
+            FBTest.progress("open Firebug");
             FBTestFirebug.openFirebug();
 
             var logRow = FW.FBL.getElementByClass(panelNode, "logRow logRow-log");
             var node = FW.FBL.getElementByClass(panelNode, "objectBox objectBox-text");
-            FBTest.compare("Hello From Test!", node.textContent, "The log must be still there.");
+            FBTest.compare("Hello From Test!", (node?node.textContent:"null node"), "The log must be still there.");
             FBTestFirebug.testDone("panelContentAfterReopen.DONE");
         });
     });
