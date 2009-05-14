01026be6795ca9b7a7c9655dc63007cb9a54a4b6
johnjbarton.com <johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-05-14 21:37:10 +0000

    reload (implicit in enableScriptPanel) so the data is updated.
    git-svn-id: http://fbug.googlecode.com/svn@2861 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/script/1425/issue1425.js
+++ tests/content/script/1425/issue1425.js
@@ -15,67 +15,72 @@ function runTest()
         if (!isOpen)
             FBTest.Firebug.pressToggleFirebug();
 
-        var panel = FW.FirebugChrome.selectPanel("script");
-
-        FBTest.progress("Navigated to "+panel.name+" panel");
-
-        // Select proper JS file.
-        var found = FBTestFirebug.selectPanelLocationByName(panel, "main.js");
-        FBTest.compare(found, true, "The main.js should be found");
-
-
-        var panel = FW.FirebugContext.chrome.getSelectedPanel();
-        var selectedLocationDescription = panel.getObjectDescription(panel.location);
-        FBTest.compare("main.js", selectedLocationDescription.name,  "The selected location must be main.js");
-
-        var sourceBox = panel.getSourceBoxByURL(panel.location.href);
-        var sourceViewport =  FW.FBL.getChildByClass(sourceBox, 'sourceViewport');
-        if (sourceViewport)
+        FBTestFirebug.enableScriptPanel(function reloadIt(win)
         {
-            var rows = sourceViewport.childNodes;
+            FBTest.progress("reloaded, ready to go");
 
-            FBTest.ok(rows.length > 1, "The script view must not be empty.");
-            if (rows.length < 1)
-                issue1425.done();
-            var source1 = "function MapLoadingIndicator(m){\n";
-            FBTest.compare(source1, rows[1].firstChild.nextSibling.textContent,
-                    "Verify source on line 1");
+            var panel = FW.FirebugChrome.selectPanel("script");
+            FBTest.progress("Navigated to "+panel.name+" panel");
 
-            // Scroll to 1143
-            FBTest.progress("Scroll to line 1143")
-            FBTest.Firebug.selectSourceLine(panel.location.href, 1143, "js");
+            // Select proper JS file.
+            var found = FBTestFirebug.selectPanelLocationByName(panel, "main.js");
+            FBTest.compare(found, true, "The main.js should be found");
 
-            var tries = 5;
-            var checking = setInterval( function checkScrolling()
+
+            var panel = FW.FirebugContext.chrome.getSelectedPanel();
+            var selectedLocationDescription = panel.getObjectDescription(panel.location);
+            FBTest.compare("main.js", selectedLocationDescription.name,  "The selected location must be main.js");
+
+            var sourceBox = panel.getSourceBoxByURL(panel.location.href);
+            var sourceViewport =  FW.FBL.getChildByClass(sourceBox, 'sourceViewport');
+            if (sourceViewport)
             {
-                    FBTest.progress("check scrolling, remaining tries: "+tries);
-                    // Look for line 1143
-                    var row1143 = FBTestFirebug.getSourceLineNode(1143);
+                var rows = sourceViewport.childNodes;
 
-                    if (!row1143 && --tries)
-                        return;
-
-                    // Check 1143
-                    FBTest.ok(row1143, "The row 1143 must exist");
-                    if (row1143)
-                    {
-                        var source1143 = "initialize:function(config){\n";
-                        FBTest.compare(source1143, row1143.firstChild.nextSibling.textContent,
-                            "The source code at line 1143 verified.");
-                    }
-                    else
-                    {
-
-                        FBTest.sysout("Where is 1143 row in "+panel.location.href, rows);
-                    }
-                    clearInterval(checking);
+                FBTest.ok(rows.length > 1, "The script view must not be empty.");
+                if (rows.length < 1)
                     issue1425.done();
-            }, 50);
-        }
-        else
-        {
-            issue1425.done();
-        }
+                var source1 = "function MapLoadingIndicator(m){\n";
+                FBTest.compare(source1, rows[1].firstChild.nextSibling.textContent,
+                        "Verify source on line 1");
+
+                // Scroll to 1143
+                FBTest.progress("Scroll to line 1143")
+                FBTest.Firebug.selectSourceLine(panel.location.href, 1143, "js");
+
+                var tries = 5;
+                var checking = setInterval( function checkScrolling()
+                {
+                        FBTest.progress("check scrolling, remaining tries: "+tries);
+                        // Look for line 1143
+                        var row1143 = FBTestFirebug.getSourceLineNode(1143, FW.Firebug.chrome);
+
+                        if (!row1143 && --tries)
+                            return;
+
+                        // Check 1143
+                        FBTest.ok(row1143, "The row 1143 must exist");
+                        if (row1143)
+                        {
+                            var source1143 = "initialize:function(config){\n";
+                            FBTest.compare(source1143, row1143.firstChild.nextSibling.textContent,
+                                "The source code at line 1143 verified.");
+                        }
+                        else
+                        {
+
+                            FBTest.sysout("Where is 1143 row in "+panel.location.href, rows);
+                        }
+                        clearInterval(checking);
+                        issue1425.done();
+                }, 50);
+            }
+            else
+            {
+                issue1425.done();
+            }
+        });
+
     });
 }
 
