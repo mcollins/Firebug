f9fc123bf032c493fa819872bfffe8ee8f7b1523
johnjbarton.com <johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-05-16 16:46:40 +0000

    rename prefs extensions.firebug-service.* to extensions.firebug.service.*
    git-svn-id: http://fbug.googlecode.com/svn@2885 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- chromebug/branches/chromebug0.4/components/chromebug_command_line.js
+++ chromebug/branches/chromebug0.4/components/chromebug_command_line.js
@@ -143,7 +143,7 @@ const  chromebugCommandLineHandler = {
                     if (launchChromebug)
                     {
                         getStartupObserver().startOnStartupNextTime();
-                        prefs.setBoolPref("extensions.firebug-service.filterSystemURLs", false);  // See firebug-service.js
+                        prefs.setBoolPref("extensions.firebug.service.filterSystemURLs", false);  // See firebug-service.js
                         chromebugCommandLineHandler.openChromebug(window);
                     }
                     else
