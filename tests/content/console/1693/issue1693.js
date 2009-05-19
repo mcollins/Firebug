49fdc58e35f75f65080835e6e1def3afbce04b1d
odvarko@gmail.com <odvarko@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-05-19 14:21:34 +0000

    Test for Issue 1693: Viewing requests with large message bodies in Console tab temporarily freezes browser
    git-svn-id: http://fbug.googlecode.com/svn@2919 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/console/1693/issue1693.js
@@ -0,0 +1,28 @@
+function runTest()
+{
+    FBTest.sysout("issue1693.START");
+
+    FBTest.registerPathHandler("/console/1693/issue1693.php", function (metadata, response)
+    {
+        response.setHeader("Content-Type", "text/html", false);
+        var text = "";
+        for (var i=0; i<80000; i++)
+            text += i + " ";
+        response.write(text);
+    });
+
+    FBTestFirebug.openNewTab(basePath + "console/1693/issue1693.html", function(win)
+    {
+        FBTestFirebug.enableConsolePanel(function(win)
+        {
+            var panelNode = FBTestFirebug.selectPanel("console").panelNode;
+            win.wrappedJSObject.executeRequest(function(request)
+            {
+                FBTestFirebug.expandElements(panelNode, "spyTitleCol", "spyCol");
+                setTimeout(function() {
+                    FBTest/*Firebug*/.testDone("issue1693.DONE");
+                }, 500);
+            });
+        });
+    });
+}
---
+++ tests/content/console/1693/issue1693.php
@@ -0,0 +1,6 @@
+<?php
+header("Content-Type: text/plain");
+for ($i=0; $i<80000; i++)
+    echo $i;
+    echo " ";
+?>
