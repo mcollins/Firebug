b2f854e6e706b72820bc153717e89f783d63986e
johnjbarton.com <johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-05-20 00:30:51 +0000

    clean up test
    git-svn-id: http://fbug.googlecode.com/svn@2928 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/firebug/viralActivation.js
+++ tests/content/firebug/viralActivation.js
@@ -27,33 +27,44 @@ function viralActivation()
         else
             FBTest.ok(false, "no FirebugContext");
 
-        var link = win.document.getElementById("sameTabOpen");
-        var tabbrowser = FW.getBrowser();
-        var browser = tabbrowser.getBrowserForTab(tabbrowser.selectedTab);
-        browser.addEventListener("load", function loadedBrowser()
-        {
-            browser.removeEventListener("load", loadedBrowser, true);
-            setTimeout(checkFBOpen);
-
-        }, true);
-
-        function checkFBOpen(event)
-        {
-            FBTest.progress("Entered checkFBOpen");
-            var url = link.getAttribute("href");
-            var doc = browser.contentWindow.document;
-            FBTest.compare(path+url, doc.location.toString(), "The url of the link and the document that opened should match");
-            var isFirebugOpen = FBTest.Firebug.isFirebugOpen();
-            FBTest.ok(isFirebugOpen, "Firebug is open on "+url);
-            FBTestFirebug.testDone("viralActivation: 1/4 completed");
-        }
-
-        FBTest.progress("Click link "+link.getAttribute('id'));
-        FBTest.click(link);
+        sameTabOpen(win, path);
 
     });
 }
 
+function sameTabOpen(win, path)
+{
+    var link = win.document.getElementById("sameTabOpen");
+    var url = link.getAttribute("href");
+    var tabbrowser = FW.getBrowser();
+    var browser = tabbrowser.getBrowserForTab(tabbrowser.selectedTab);
+    browser.addEventListener("load", function loadedBrowser()
+    {
+        browser.removeEventListener("load", loadedBrowser, true);
+        setTimeout(function checkFBOpen(event)
+        {
+            FBTest.progress("Entered checkFBOpen");
+
+            var doc = browser.contentWindow.document;
+            FBTest.compare(path+url, doc.location.toString(), "The url of the link and the document that opened should match");
+            var placement = FW.Firebug.getPlacement();
+            FBTest.compare("inBrowser", placement, "Firebug is placed in browser");
+            var suspension = FW.Firebug.getSuspended();
+            FBTest.compare(null, suspension, "Firebug is not suspended on "+browser.currentURI.spec);
+
+            FBTest.progress("Go back to the first page");
+            browser.contentWindow.back();
+
+            var suspension = FW.Firebug.getSuspended();
+            FBTest.compare(null, suspension, "Firebug is not suspended on "+browser.currentURI.spec);
+            FBTestFirebug.testDone("viralActivation: 1/4 completed");
+        });
+
+    }, true);
+
+    FBTest.progress("Click link "+link.getAttribute('id'));
+    FBTest.click(link);
+}
 
 
 //------------------------------------------------------------------------
