19258def4ab899d0c49a1abee7397f2f12812aa4
odvarko@gmail.com <odvarko@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-06-29 08:19:16 +0000

    Test for issue 1693, verify also displayed text content.
    git-svn-id: http://fbug.googlecode.com/svn@3351 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/console/1693/issue1693.js
+++ tests/content/console/1693/issue1693.js
@@ -17,14 +17,29 @@ function runTest()
     {
         FBTestFirebug.enableConsolePanel(function(win)
         {
+            FBTest.progress("issue1693.Select the Console panel and execute large request.");
+
             var panelNode = FBTestFirebug.selectPanel("console").panelNode;
             win.wrappedJSObject.executeRequest(function(request)
             {
                 FBTest.progress("Request call back called");
-                FBTestFirebug.expandElements(panelNode, "spyTitleCol", "spyCol");
-                FBTest.ok(true, "Completing the functions is the test?");
-                FBTest.testDone("issue1693.DONE");
 
+                // Expand XHR entry within the Console panel. The browser must not freeze
+                // and the response body must be properly displayed.
+                FBTestFirebug.expandElements(panelNode, "spyTitleCol", "spyCol");
+
+                // Get response body element and check its content. Note that the displayed text
+                // is limited in case of large responses.
+                var limit = FBTestFirebug.getPref("net.displayedResponseLimit");
+                var responseBody = FW.FBL.getElementsByClass(panelNode,
+                    "netInfoResponseText", "netInfoText");
+
+                // Compare expected and actuall (displayed) response text.
+                var text1 = request.responseText.substr(0, limit);
+                var text2 = responseBody[0].textContent.substr(0, limit);
+                FBTest.compare(text1, text2, "The response text must be properly displayed");
+
+                FBTestFirebug.testDone("issue1693.DONE");
             });
         });
     });
