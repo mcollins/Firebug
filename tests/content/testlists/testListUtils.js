56776f3a5f154487284bddaaa407973793451d70
odvarko@gmail.com <odvarko@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-09-07 11:36:47 +0000

    Create test list for Firebug 1.4 loadable as a web page + share javascript code in testListUtils.js.
    git-svn-id: http://fbug.googlecode.com/svn@4233 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/testlists/testListUtils.js
@@ -0,0 +1,35 @@
+function getDriverBaseURI()
+{
+    var parts = window.location.toString().split('/');
+    parts.pop(); // remove file name
+    parts.pop(); // remove /testLists/
+    return parts.join('/') + "/";
+}
+
+window.addEventListener("load", function showPage()
+{
+    var d = document.getElementById("driverURI");
+    if (d)
+        d.innerHTML = driverBaseURI;
+
+    var t = document.getElementById("testcaseURI");
+    if (t)
+        t.innerHTML = serverURI;
+
+    var cases = document.getElementById("testList");
+    var currentGroup = null;
+    for(var i = 0; i < testList.length; i++)
+    {
+        var testCase = testList[i];
+        if (testCase.group != currentGroup)
+        {
+            currentGroup = testCase.group;
+            var h3 = document.createElement('h3');
+            h3.innerHTML = currentGroup;
+            cases.appendChild(h3);
+        }
+        var entry = document.createElement('div');
+        entry.innerHTML="<a href="+driverBaseURI+testCase.uri+">"+testCase.desc+"</a>";
+        cases.appendChild(entry);
+    }
+}, true);
