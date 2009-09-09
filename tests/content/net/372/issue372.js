0694b8e3005870669503d3696c98c17cf2220ced
odvarko@gmail.com <odvarko@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-09-09 11:38:24 +0000

    Fix test 372 (a css class has been changed)
    git-svn-id: http://fbug.googlecode.com/svn@4258 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/net/372/issue372.js
+++ tests/content/net/372/issue372.js
@@ -15,10 +15,10 @@ function runTest(request)
                 FBTestFirebug.expandElements(panelNode, "netInfoPostTab");
 
                 // The post text must be displayed.
-                var postBody = FW.FBL.getElementByClass(panelNode, "netInfoPostTable");
+                var postBody = FW.FBL.getElementByClass(panelNode, "netInfoPostSourceTable");
                 if (FBTest.ok(postBody, "Post tab must exist."))
                 {
-                    var row = postBody.firstChild.childNodes[2];
+                    var row = postBody.firstChild.childNodes[1];
                     FBTest.compare(win.wrappedJSObject.xml, row.textContent, 
                         "Post tab body content verified");
                 }
