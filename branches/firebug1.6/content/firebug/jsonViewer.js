9ba1662d0ded2385d89172ed5417148ee9eb5e4c
johnjbarton.com <johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-10-22 05:13:58 +0000

    Branch at R4673 to create Firebug 1.6
    git-svn-id: http://fbug.googlecode.com/svn@4674 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ branches/firebug1.6/content/firebug/jsonViewer.js
@@ -0,0 +1,90 @@
+/* See license.txt for terms of usage */
+
+FBL.ns(function() { with (FBL) {
+
+// ************************************************************************************************
+// Model implementation
+
+Firebug.JSONViewerModel = extend(Firebug.Module,
+{
+    dispatchName: "jsonViewer",
+    initialize: function()
+    {
+        Firebug.NetMonitor.NetInfoBody.addListener(this);
+
+        // Used by Firebug.DOMPanel.DirTable domplate.
+        this.toggles = {};
+    },
+
+    shutdown: function()
+    {
+        Firebug.NetMonitor.NetInfoBody.removeListener(this);
+    },
+
+    initTabBody: function(infoBox, file)
+    {
+        if (FBTrace.DBG_JSONVIEWER)
+            FBTrace.sysout("jsonviewer.initTabBody", infoBox);
+
+        // Let listeners to parse the JSON.
+        dispatch(this.fbListeners, "onParseJSON", [file]);
+
+        // The JSON is still no there, try to parse most common cases.
+        if (!file.jsonObject)
+        {
+            const maybeHarmful = /[^,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]/;
+            const jsonStrings = /"(\\.|[^"\\\n\r])*"/g;
+
+            var contentType = safeGetContentType(file.request);
+            if (!contentType)
+                return;
+
+            if ((contentType.indexOf("application/json") != 0) &&
+                (contentType.indexOf("text/plain") != 0) &&
+                (contentType.indexOf("text/x-json") != 0) &&
+                (contentType.indexOf("text/javascript") != 0))
+                return;
+
+            file.jsonObject = this.parseJSON(file);
+        }
+
+        // The jsonObject is created so, the JSON tab can be displayed.
+        if (file.jsonObject)
+        {
+            Firebug.NetMonitor.NetInfoBody.appendTab(infoBox, "JSON",
+                $STR("jsonviewer.tab.JSON"));
+
+            if (FBTrace.DBG_JSONVIEWER)
+                FBTrace.sysout("jsonviewer.initTabBody; JSON object available", file.jsonObject);
+        }
+    },
+
+    // Update listener for TabView
+    updateTabBody: function(infoBox, file, context)
+    {
+        var tab = infoBox.selectedTab;
+        var tabBody = getElementByClass(infoBox, "netInfoJSONText");
+        if (!hasClass(tab, "netInfoJSONTab") || tabBody.updated)
+            return;
+
+        tabBody.updated = true;
+
+        if (file.jsonObject) {
+            Firebug.DOMPanel.DirTable.tag.replace(
+                 {object: file.jsonObject, toggles: this.toggles}, tabBody);
+        }
+    },
+
+    parseJSON: function(file)
+    {
+        var jsonString = new String(file.responseText);
+        return parseJSONString(jsonString, "http://" + file.request.originalURI.host);
+    },
+
+});
+
+// ************************************************************************************************
+// Registration
+
+Firebug.registerModule(Firebug.JSONViewerModel);
+}});
