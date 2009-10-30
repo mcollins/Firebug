a2e0265746cca6f7455f080fce3904e85d07102d
kpdecker <kpdecker@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-10-30 03:11:52 +0000

    Initial import
    git-svn-id: http://fbug.googlecode.com/svn@4739 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ extensions/firefocus/branches/firefocus1.0/content/firefocus/firefocus.js
@@ -0,0 +1,125 @@
+/* See license.txt for terms of usage */
+FBL.ns(function() { with (FBL) {
+
+const FOCUSED_CLASS = "contentFocused";
+
+var i18n = document.getElementById("strings_firefocus");
+
+Firebug.FocusModule = extend(Firebug.Module, {
+  //////////////////////////////////////////////
+  // Module
+  initContext: function(context, persistedState) {
+    this.monitorContext(context);
+  },
+  destroyContext: function(context, persistedState) {
+    this.unmonitorContext(context);
+  },
+  showPanel: function(browser, panel) {
+    if (panel.name == "html" || panel.name == "console") {
+      this.addStyleSheet(panel.document);
+    }
+  },
+
+  //////////////////////////////////////////////
+  // Self
+  focusLogger: function(event, context) {
+    var panel = context.getPanel("html", true),
+        target = event.target;
+
+    if (this.getLogFocusPref() && Firebug.Console.isAlwaysEnabled()) {
+      Firebug.Console.logFormatted([i18n.getString("log.FocusIn"), target], context, "focusIn", true, null);
+    }
+
+    if (panel && target.parentNode) {
+      var objectBox = Firebug.scrollToMutations || Firebug.expandMutations
+          ? panel.ioBox.createObjectBox(target)
+          : panel.ioBox.findObjectBox(target);
+      this.highlightFocus(objectBox, objectBox, FOCUSED_CLASS, panel);
+    }
+  },
+  blurLogger: function(event, context) {
+    var panel = context.getPanel("html", true),
+        target = event.target;
+
+    if (this.getLogFocusPref() && Firebug.Console.isAlwaysEnabled()) {
+      Firebug.Console.logFormatted([i18n.getString("log.FocusOut"), target], context, "focusOut", true, null);
+    }
+
+    if (panel) {
+      var focused = panel.panelNode.getElementsByClassName(FOCUSED_CLASS);
+      for (var i = focused.length; i > 0; i--) {
+        removeClass(focused[i-1], FOCUSED_CLASS);
+      }
+    }
+  },
+
+  // Much of this logic mirors the Firebug bug implementation of HTMLPanel.highlightFocus
+  // This has the slight differences that rather than using a class timeout, the class
+  // remains on until blur is called.
+  highlightFocus: function(elt, objectBox, type, panel) {
+    if (!elt || !Firebug.getPref(Firebug.prefDomain, "firefocus.highlightFocus"))
+        return;
+
+    var ioBox = panel.ioBox,
+        panelNode = panel.panelNode,
+        context = panel.context;
+
+    if (Firebug.scrollToMutations || Firebug.expandMutations) {
+      if (context.focusTimeout) {
+          context.clearTimeout(context.focusTimeout);
+          delete context.focusTimeout;
+      }
+
+      context.focusTimeout = context.setTimeout(function() {
+        ioBox.openObjectBox(objectBox);
+
+        if (Firebug.scrollToMutations)
+          scrollIntoCenterView(objectBox, panelNode);
+      }, 200);
+    }
+
+    setClass(elt, type);
+  },
+
+  getLogFocusPref: function() {
+    return Firebug.getPref(Firebug.prefDomain, "firefocus.logFocus");
+  },
+
+  addStyleSheet: function(doc) {
+    // Make sure the stylesheet isn't appended twice.
+    var id = "fireFocusCss";
+    if ($(id, doc))   return;
+
+    var styleSheet = createStyleSheet(doc, "chrome://firefocus/skin/firefocus.css");
+    styleSheet.setAttribute("id", id);
+    addStyleSheet(doc, styleSheet);
+  },
+  monitorContext: function(context) {
+    var focusContext = this.getFocusContext(context);
+    if (focusContext.focusLogger)    return;
+
+    focusContext.focusLogger = bind(this.focusLogger, this, context);
+    focusContext.blurLogger = bind(this.blurLogger, this, context);
+
+    context.window.addEventListener("focus", focusContext.focusLogger, true);
+    context.window.addEventListener("blur", focusContext.blurLogger, true);
+  },
+  unmonitorContext: function(context) {
+    var focusContext = this.getFocusContext(context);
+    if (!focusContext.focusLogger)    return;
+
+    context.window.removeEventListener("focus", focusContext.focusLogger, true);
+    context.window.removeEventListener("blur", focusContext.blurLogger, true);
+
+    delete focusContext.focusLogger;
+    delete focusContext.blurLogger;
+  },
+  getFocusContext: function(context) {
+    context.focusContext = context.focusContext || {};
+    return context.focusContext;
+  }
+});
+
+Firebug.registerModule(Firebug.FocusModule);
+
+}});
