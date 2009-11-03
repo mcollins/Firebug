fb5cc4265dae1d8ab55efbf3513c1c025389a5b5
odvarko@gmail.com <odvarko@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-11-03 13:10:34 +0000

    Fix compatibility issue with test console/testErrors
    git-svn-id: http://fbug.googlecode.com/svn@4755 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/console/testErrors1.4.js
+++ tests/content/console/testErrors1.4.js
@@ -18,7 +18,7 @@ function fireTest(win, ith)
     var buttons = ["syntaxError", "shallowError", "deepError", "throw", "uncaughtException1891"];
     var titles = ["missing ; before statement", "foops is not defined",
                   "B3 is not defined", "uncaught exception: hi", 'String contains an invalid character"  code: "5'];
-    var sources = ["2BeOrNot2Be(40)", "", "/*foo*/                    B3();", "", ""];
+    var sources = ["2BeOrNot2Be(40)", "", "/*foo*/                    B3();\\r\\n", "", ""];
 
     if (ith >= buttons.length)
     {
