4481dc745bcec232156213abfb543f6afe7fb69c
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-11-23 19:32:08 +0000

    FBTest for Issue #2462: The firebug console still shows the xhr in progress if you abort it (via request.abort())
    git-svn-id: http://fbug.googlecode.com/svn@4938 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/console/spy/2462/issue2462.js
@@ -0,0 +1,27 @@
+function runTest()
+{
+    FBTest.sysout("issue2462.START");
+
+    var prefOrigValue = FBTestFirebug.getPref("showXMLHttpRequests");
+    FBTestFirebug.setPref("showXMLHttpRequests", true);
+
+    FBTestFirebug.openNewTab(basePath + "console/spy/2462/issue2462.html", function(win)
+    {
+        FBTestFirebug.enableConsolePanel(function()
+        {
+            var panel = FW.FirebugChrome.selectPanel("console");
+            win.wrappedJSObject.onExecuteTest(function(request)
+            {
+                var rows = FW.FBL.getElementsByClass(panel.panelNode,
+                    "logRow", "logRow-spy", "error", "loaded");
+
+                FBTest.compare(1, rows.length,
+                    "There must be one aborted and finished XHR.");
+
+                // Finish test
+                FBTestFirebug.setPref("showXMLHttpRequests", prefOrigValue);
+                FBTestFirebug.testDone("issue2462.DONE");
+            });
+        });
+    });
+}
