8c9d0b72bd0177eebcb1616a3b3b373ea46ecc77
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-12-21 19:24:52 +0000

    FBTest examples: exampleNet1
    git-svn-id: http://fbug.googlecode.com/svn@5418 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/examples/exampleNet1.js
@@ -0,0 +1,37 @@
+function runTest()
+{
+    FBTest.sysout("exampleNet1.START");
+
+    // 1) Load test case page
+    FBTestFirebug.openNewTab(basePath + "examples/exampleNet1.html", function(win)
+    {
+        // 2) Open Firebug and enable the Net panel.
+        FBTestFirebug.openFirebug();
+        FBTestFirebug.enableNetPanel(function(win)
+        {
+            // 3) Select Net panel
+            var panel = FW.FirebugChrome.selectPanel("net");
+
+            // Asynchronously wait for the request beeing displayed.
+            onRequestDisplayed(function(netRow)
+            {
+                // 5) Finish test
+                FBTestFirebug.testDone("exampleNet1.DONE");
+            });
+
+            // 4) Execute test by clicking on the 'Execute Test' button.
+            FBTest.click(win.document.getElementById("testButton"));
+        });
+    });
+}
+
+function onRequestDisplayed(callback)
+{
+    // Create listener for mutation events.
+    var doc = FBTestFirebug.getPanelDocument();
+    var recognizer = new MutationRecognizer(doc.defaultView, "tr",
+        {"class": "netRow category-xhr loaded"});
+
+    // Wait for a XHR log to appear in the Net panel.
+    recognizer.onRecognizeAsync(callback);
+}
