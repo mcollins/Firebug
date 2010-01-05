11360b2263a9dc601a4e60d2b6efd4924db3ea30
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-01-05 09:10:51 +0000

    Another try to get sleep working.
    git-svn-id: http://fbug.googlecode.com/svn@5549 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/issues/bugzilla/b537762/sleep-test-do-not-cache.php
@@ -0,0 +1,18 @@
+<?php
+set_time_limit(10);
+sleep(2);   // Sleep for 2 sec.
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+    <title>Test of the sleep() function</title>
+    <meta http-equiv="Pragma" content="no-cache"></meta>
+    <meta HTTP-equiv="Expires" content="-1"></meta>
+</head>
+<body>
+
+<h1>Test of the sleep() function</h1>
+<p>This page should load in 2 sec.</p>
+
+</body>
+</html>
