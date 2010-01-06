d612f22323228b771c019c9daecf0d75c902aa2e
johnjbarton.com <johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-01-06 01:56:01 +0000

    swarm menu item added, but not triggered by selection
    git-svn-id: http://fbug.googlecode.com/svn@5558 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- extensions/swarm/branches/swarm1.5/content/swarm.js
+++ extensions/swarm/branches/swarm1.5/content/swarm.js
@@ -24,18 +24,21 @@ Firebug.registerStringBundle("chrome://swarm/locale/swarm.properties");
 // ************************************************************************************************
 Firebug.MenuItem = {
 
-    createMainMenuItem: function(name, before)
+    createMainMenuItem: function(name, beforeID, controller)
     {
         var mainMenu = document.getElementById("fbFirebugMenuPopup");
         var item =
         {
             label: $STR(name+"_label"),
-            id: "fbMenuItem_"+name,
-            command: bind(this.onSelect, this),
+            command: bind(controller.onSelect, controller),
             key: "fbKey_"+name,
         }
-        var commandSet = document.getElementById('mainCommandSet');
-        return createMenuItem(mainMenu, item, before);
+        if (beforeID)
+            var before = document.getElementById(beforeID);
+        var mainMenuItem = createMenuItem(mainMenu, item, before);
+        mainMenuItem.setAttribute("id", "fbMenuItem_"+name);
+
+        return mainMenuItem;
     },
 
     getLastMenuItemAfter: function(searchID)
@@ -50,23 +53,69 @@ Firebug.MenuItem = {
 /**
  * @menuitem "Manage Firebug Extensions"
  */
-function SwarmEntry() {}
-SwarmEntry.prototype = extend(Firebug.MenuItem,
-/** @lends EventPanel */
+var SwarmEntry = extend(Firebug.MenuItem,
 {
     name: "swarm",
 
-    onPopupShowing: function()
+    initialize: function()
     {
-        // activiate if the gBrowser page is a swarm definition page
+        this.mainMenuItem = this.createMainMenuItem(this.name, "menu_aboutSeparator", this);
     },
 
-    onSelect: function()
+    onSelect: function(event)
     {
+        var doc = context.window.document;
+        var buttons = doc.getElementsByClassName("swarmInactive");
+        if (buttons.length)
+        {
+            for (var i = 0; i < buttons.length; i++ )
+            {
+                removeClass(buttons[i], "swarmInactive");
+                setClass(buttons[i], "swarmActive");
+                buttons[i].addEventListener('click', FirebugSwarm.install, true);
+            }
+        }
+        else
+        {
+            FBTrace.sysout("FirebugSwarm: need to notify or prevent this case.");
+        }
 
     },
+});
+
+Firebug.registerMenuItem(SwarmEntry);
+
+var FirebugSwarm =
+{
+    install: function(event)
+    {
+        var button = event.target;
+        var swarms = document.getElementsByClassName("firebug signed swarm");
+        var iSwarm = -1;
+        for (var i  = 0; i  < swarms.length; i++)
+            if (swarms[i] === button.parentNode)
+                iSwarm = i;
+
+        if (iSwarm >= 0)
+        {
+            var swarm = swarms[iSwarm];
+            event.target.innerHTML = "Check Signature";
+            var extensions = swarm.getElementsByClassName("extensionURL");
+            var extensionURLs = [];
+            var extensionHash = [];
+            for (var i = 0; i < extensions.length; i++)
+            {
+                extensionURLs[i] = extensions[i].getAttribute("href");
+                extensionHash[i] = extensions[i].getAttribute("hash");
+            }
+            event.target.innerHTML = "Ready for "+extensions.length+" extensions";
+        }
+        else
+        {
+            event.target.innerHTML = "No valid swarm found";
+        }
+    },
+
 }
-//    Firebug.registerMenuItem(SwarmEntry);
-
 // ************************************************************************************************
 }});
