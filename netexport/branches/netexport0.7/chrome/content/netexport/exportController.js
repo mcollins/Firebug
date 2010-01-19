3d72cb9df3b34e8782a8ee0785addd0dcfbc0cbe
odvarko@gmail.com <odvarko@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-01-19 17:17:34 +0000

    New icons and button for auto-export of Net panel data + simple refactoring + localization.
    git-svn-id: http://fbug.googlecode.com/svn@5744 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ netexport/branches/netexport0.7/chrome/content/netexport/exportController.js
@@ -0,0 +1,20 @@
+/* See license.txt for terms of usage */
+
+FBL.ns(function() { with (FBL) {
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+
+// ************************************************************************************************
+//
+
+Firebug.NetExport.Controller = extend(Firebug.Module,
+{
+    initialize: function(owner)
+    {
+        
+    },
+});
+
+// ************************************************************************************************
+}});
