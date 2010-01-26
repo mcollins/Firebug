fea1cdfb41d07063e7f28d3a4f9d8a7c4f0005f5
odvarko@gmail.com <odvarko@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-01-26 09:32:37 +0000

    Don't trace by default.
    git-svn-id: http://fbug.googlecode.com/svn@5844 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- netexport/branches/netexport0.7/defaults/preferences/prefs.js
+++ netexport/branches/netexport0.7/defaults/preferences/prefs.js
@@ -1,5 +1,5 @@
 // Firebug tracing support
-pref("extensions.firebug.DBG_NETEXPORT", true);
+pref("extensions.firebug.DBG_NETEXPORT", false);
 pref("extensions.firebug.netexport.viewerURL", "http://www.softwareishard.com/har/viewer-1.1");
 pref("extensions.firebug.netexport.compress", false);
 pref("extensions.firebug.netexport.defaultLogDir", "");
