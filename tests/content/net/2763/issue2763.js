b8ed5431dea59b67d2a35d6650690e0dcb3ba312
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-02-09 16:15:28 +0000

    FBTest for Issue 2755: Firebug crash FF when upload a file when firebug is active
    git-svn-id: http://fbug.googlecode.com/svn@6027 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/net/2763/issue2763.js
@@ -0,0 +1,39 @@
+function runTest()
+{
+    FBTest.sysout("issue2763.START");
+
+    // Load test case page
+    FBTestFirebug.openNewTab(basePath + "net/2763/issue2763.html", function(win)
+    {
+        // Open Firebug and enable the Net panel.
+        FBTestFirebug.openFirebug();
+        FBTestFirebug.enableNetPanel(function(win)
+        {
+            // Select Net panel
+            var panel = FW.FirebugChrome.selectPanel("net");
+
+            // Wait for the only request that should be displayed in the Net panel.
+            onRequestDisplayed(function(netRow)
+            {
+                // Finish test, if Firefox hasn't crashed by now, all is OK.
+                FBTestFirebug.testDone("issue2763.DONE");
+            });
+
+            // Execute test by clicking on the 'Execute Test' button.
+            FBTest.click(win.document.getElementById("testButton"));
+
+            FBTest.progress("Test button clicked");
+        });
+    });
+}
+
+function onRequestDisplayed(callback)
+{
+    // Create listener for mutation events.
+    var doc = FBTestFirebug.getPanelDocument();
+    var recognizer = new MutationRecognizer(doc.defaultView, "tr",
+        {"class": "netRow category-xhr hasHeaders loaded"});
+
+    // Wait for a XHR log to appear in the Net panel.
+    recognizer.onRecognizeAsync(callback);
+}
