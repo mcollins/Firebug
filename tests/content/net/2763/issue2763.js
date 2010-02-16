5b466c6da942351fa96a063da99d98ddf92b2cda
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-02-16 14:25:36 +0000

    Finish upload test on timeout, which is enough to reproduce the problem.
    git-svn-id: http://fbug.googlecode.com/svn@6109 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/net/2763/issue2763.js
+++ tests/content/net/2763/issue2763.js
@@ -12,16 +12,24 @@ function runTest()
             // Select Net panel
             var panel = FW.FirebugChrome.selectPanel("net");
 
+            // The upload can take more time on slower connetions, so wait
+            // for 5 sec at most, which is enough to repro the problem.
+            var timeoutID = setTimeout(function() {
+                FBTest.progress("Test finished on timeout.");
+                FBTestFirebug.testDone("issue2763.DONE");
+            }, 5000);
+
             // Wait for the only request that should be displayed in the Net panel.
             onRequestDisplayed(function(netRow)
             {
+                clearTimeout(timeoutID);
+
                 // Finish test, if Firefox hasn't crashed by now, all is OK.
                 FBTestFirebug.testDone("issue2763.DONE");
             });
 
             // Execute test by clicking on the 'Execute Test' button.
             FBTest.click(win.document.getElementById("testButton"));
-
             FBTest.progress("Test button clicked");
         });
     });
