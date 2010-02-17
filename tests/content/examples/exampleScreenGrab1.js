e9b32fbd0558e86d4f16998275ba6612985ed178
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-02-17 10:57:24 +0000

    Slightly modified FBTest example for screen grab.
    git-svn-id: http://fbug.googlecode.com/svn@6120 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/examples/exampleScreenGrab1.js
+++ tests/content/examples/exampleScreenGrab1.js
@@ -5,6 +5,11 @@ function runTest()
     {
         FBTestFirebug.openFirebug();
 
+        // Helper code for creating the image file.
+        //var file = pickFile();
+        //if (file)
+        //    FBTestFirebug.saveWindowImageToFile(win, 100, 100, file.path);
+
         var imgURL = basePath + "examples/exampleScreenGrab1.png";
         FBTestFirebug.loadImageData(imgURL, function(expectedImage)
         {
@@ -13,4 +18,21 @@ function runTest()
             FBTestFirebug.testDone("exampleScreenGrab1.DONE");
         })
     });
+}
+
+function pickFile()
+{
+    var nsIFilePicker = Ci.nsIFilePicker;
+    var fp = Cc["@mozilla.org/filepicker;1"].getService(nsIFilePicker);
+    fp.init(window, null, nsIFilePicker.modeSave);
+    fp.appendFilter("PNG Images","*.png;");
+    fp.appendFilters(nsIFilePicker.filterAll);
+    fp.filterIndex = 1;
+    fp.defaultString = "exampleScreenGrab1.png";
+
+    var rv = fp.show();
+    if (rv == nsIFilePicker.returnOK || rv == nsIFilePicker.returnReplace)
+        return fp.file;
+
+    return null;
 }
