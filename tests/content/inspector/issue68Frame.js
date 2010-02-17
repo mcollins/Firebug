fc3e7049ee603d60fd98c70321bd77e2c8feafb0
sabine.michael.ratcliffe <sabine.michael.ratcliffe@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-02-17 21:48:10 +0000

    Added Inspector tests for issue 68
    git-svn-id: http://fbug.googlecode.com/svn@6131 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/inspector/issue68Frame.js
@@ -0,0 +1,27 @@
+function runTest()
+{
+    FBTest.sysout("issue68Frame.START");
+        
+    FBTestFirebug.openNewTab(basePath + "inspector/Issue68FrameExpected.htm", function(win)
+    {
+        var actualImage, expectedImage,
+            width = win.document.body.clientWidth,
+            height = 200;
+            
+        // To get full html for expected page: win.document.documentElement.innerHTML
+        expectedImage = FBTestFirebug.getImageDataFromWindow(win, width, height);
+        
+        FBTestFirebug.openURL(basePath + "inspector/Issue68FrameActual.htm", function(win)
+        {
+            FBTestFirebug.openFirebug();
+
+            var target = win.document.getElementById("testTarget1");
+            FBTestFirebug.inspectUsingFrame(target);
+            
+            actualImage = FBTestFirebug.getImageDataFromWindow(win, width, height);
+           
+            FBTest.compare(expectedImage, actualImage, "The screen must be in expected state");
+            FBTestFirebug.testDone("issue68Frame.DONE");
+        });
+    });
+}
