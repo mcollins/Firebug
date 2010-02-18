a3be2b056cb6efa66c77be1a5b6cd3cacb8c3447
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-02-18 09:48:07 +0000

    Revert changes made at R6125.
    git-svn-id: http://fbug.googlecode.com/svn@6140 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/console/1693/issue1693.js
+++ tests/content/console/1693/issue1693.js
@@ -10,44 +10,28 @@ function runTest()
         {
             FBTest.progress("issue1693.Select the Console panel and execute large request.");
 
-            FBTestFirebug.selectPanel("console");
-            onRequestDisplayed(function(spyLogRow)
+            var panelNode = FBTestFirebug.selectPanel("console").panelNode;
+            win.wrappedJSObject.executeRequest(function(request)
             {
-                FBTest.progress("Request displayed");
-                var panelNode = FBTestFirebug.getPanel("console").panelNode;
+                FBTest.progress("Request call back called");
 
                 // Expand XHR entry within the Console panel. The browser must not freeze
                 // and the response body must be properly displayed.
-                var spyTitle = panelNode.getElementsByClassName("spyTitleCol spyCol").item(0);
-                FBTest.click(spyTitle);
-
-                var spyHead = panelNode.getElementsByClassName("spyHead").item(0);
-                var responseText = spyHead.repObject.xhrRequest.responseText;
+                FBTestFirebug.expandElements(panelNode, "spyTitleCol", "spyCol");
 
                 // Get response body element and check its content. Note that the displayed text
                 // is limited in case of large responses.
-                var responseBody = panelNode.getElementsByClassName(
-                    "netInfoResponseText netInfoText").item(0);
-                
                 var limit = FBTestFirebug.getPref("netDisplayedResponseLimit");
+                var responseBody = FW.FBL.getElementsByClass(panelNode,
+                    "netInfoResponseText", "netInfoText");
 
-                // Compare expected and actual (displayed) response text.
-                var text1 = responseText.substr(0, limit);
-                var text2 = responseBody.textContent.substr(0, limit);
+                // Compare expected and actuall (displayed) response text.
+                var text1 = request.responseText.substr(0, limit);
+                var text2 = responseBody[0].textContent.substr(0, limit);
                 FBTest.compare(text1, text2, "The response text must be properly displayed");
 
                 FBTestFirebug.testDone("issue1693.DONE");
             });
-
-            FBTest.click(win.document.getElementById("testButton"));
         });
     });
-}
-
-function onRequestDisplayed(callback)
-{
-    var doc = FBTestFirebug.getPanelDocument();
-    var recognizer = new MutationRecognizer(doc.defaultView, "div",
-        {"class": "logRow logRow-spy loaded"});
-    recognizer.onRecognizeAsync(callback, 2000);
 }
