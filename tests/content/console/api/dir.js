4a8a40c5ef70ad4b22ab752745f56612c117b087
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-03-01 15:38:33 +0000

    Use only FBTest namespace in this test (to verify R6215)
    git-svn-id: http://fbug.googlecode.com/svn@6216 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/console/api/dir.js
+++ tests/content/console/api/dir.js
@@ -2,13 +2,13 @@ function runTest()
 {
     FBTest.sysout("console.dir.START");
 
-    FBTestFirebug.openNewTab(basePath + "console/api/dir.html", function(win)
+    FBTest.openNewTab(basePath + "console/api/dir.html", function(win)
     {
-        FBTestFirebug.openFirebug();
-        FBTestFirebug.enableConsolePanel(function(win)
+        FBTest.openFirebug();
+        FBTest.enableConsolePanel(function(win)
         {
             var config = {tagName: "div", classes: "logRow logRow-dir"};
-            FBTestFirebug.waitForDisplayedElement("console", config, function(row)
+            FBTest.waitForDisplayedElement("console", config, function(row)
             {
                 var name = row.getElementsByClassName("memberLabelCell")[0];
                 var value = row.getElementsByClassName("memberValueCell")[0];
@@ -16,7 +16,7 @@ function runTest()
                 FBTest.compare("a", name.textContent, "The variable name must be: 'a'");
                 FBTest.compare("\"b\"", value.textContent, "The variable value must be: 'b'");
 
-                FBTestFirebug.testDone("console.dir.DONE");
+                FBTest.testDone("console.dir.DONE");
             });
 
             // Execute test implemented on the test page.
