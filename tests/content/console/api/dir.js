01f3238de4a806354fded6c7ee9db1e0c8df84b3
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-03-01 10:09:23 +0000

    FBTest for console.dir API (fails now due to a bug in Firebug).
    git-svn-id: http://fbug.googlecode.com/svn@6211 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/console/api/dir.js
@@ -0,0 +1,26 @@
+function runTest()
+{
+    FBTest.sysout("console.dir.START");
+
+    FBTestFirebug.openNewTab(basePath + "console/api/dir.html", function(win)
+    {
+        FBTestFirebug.openFirebug();
+        FBTestFirebug.enableConsolePanel(function(win)
+        {
+            var config = {tagName: "div", classes: "logRow logRow-dir"};
+            FBTestFirebug.waitForDisplayedResponse("console", config, function(row)
+            {
+                var name = row.getElementsByClassName("memberLabelCell")[0];
+                var value = row.getElementsByClassName("memberValueCell")[0];
+
+                FBTest.compare("a", name.textContent, "The variable name must be: 'a'");
+                FBTest.compare("\"b\"", value.textContent, "The variable value must be: 'b'");
+
+                FBTestFirebug.testDone("console.dir.DONE");
+            });
+
+            // Execute test implemented on the test page.
+            FBTest.click(win.document.getElementById("testButton"));
+        });
+    });
+}
