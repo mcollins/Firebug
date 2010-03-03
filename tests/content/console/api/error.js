e7fa1e3ce7c79ef73e43c78dbd6ff8fa7103b12c
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-03-03 14:08:30 +0000

    FBTest for console.error method.
    git-svn-id: http://fbug.googlecode.com/svn@6262 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/console/api/error.js
@@ -0,0 +1,32 @@
+function runTest()
+{
+    FBTest.sysout("console.error.START");
+    FBTest.openNewTab(basePath + "console/api/error.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableConsolePanel(function(win)
+        {
+            var config = {tagName: "div", classes: "logRow logRow-errorMessage"};
+            FBTest.waitForDisplayedElement("console", config, function(row)
+            {
+                // Verify displayed text.
+                var reTextContent = /This is a test error\s*console.error\(\"This is a test error\"\);\\r\\nerror\.html\s*\(line 27\)/;
+                FBTest.compare(reTextContent, row.textContent, "Text content must match.");
+
+                // Show stack trace.
+                var objectBox = row.getElementsByClassName("errorTitle")[0];
+                FBTest.click(objectBox);
+
+                // Verify the first stack frame.
+                var stackFrame = row.getElementsByClassName("objectBox-stackFrame")[0];
+                FBTest.compare(/onExecuteTest\(\)\s*error.html\s*\(line 27\)/,
+                    stackFrame.textContent, "Stack frame content must match.");
+
+                // Finish test
+                FBTest.testDone("console.error.DONE");
+            });
+
+            FBTest.click(win.document.getElementById("testButton"));
+        });
+    });
+}
