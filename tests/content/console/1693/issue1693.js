569e6ac866a0ae2dd11519f73db71b20a111a8e2
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-03-17 17:45:02 +0000

    Fix test so its working on Fx3.7
    git-svn-id: http://fbug.googlecode.com/svn@6347 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/console/1693/issue1693.js
+++ tests/content/console/1693/issue1693.js
@@ -9,29 +9,62 @@ function runTest()
         FBTestFirebug.enableConsolePanel(function(win)
         {
             FBTest.progress("issue1693.Select the Console panel and execute large request.");
+            FBTestFirebug.selectPanel("console").panelNode;
 
-            var panelNode = FBTestFirebug.selectPanel("console").panelNode;
-            win.wrappedJSObject.executeRequest(function(request)
+            onRequestDisplayed(function(row)
             {
                 FBTest.progress("Request call back called");
 
                 // Expand XHR entry within the Console panel. The browser must not freeze
                 // and the response body must be properly displayed.
-                FBTestFirebug.expandElements(panelNode, "spyTitleCol", "spyCol");
+                FBTestFirebug.expandElements(row, "spyTitleCol", "spyCol");
 
                 // Get response body element and check its content. Note that the displayed text
                 // is limited in case of large responses.
                 var limit = FBTestFirebug.getPref("netDisplayedResponseLimit");
-                var responseBody = FW.FBL.getElementsByClass(panelNode,
+                var responseBody = FW.FBL.getElementsByClass(row,
                     "netInfoResponseText", "netInfoText");
 
-                // Compare expected and actuall (displayed) response text.
-                var text1 = request.responseText.substr(0, limit);
-                var text2 = responseBody[0].textContent.substr(0, limit);
-                FBTest.compare(text1, text2, "The response text must be properly displayed");
+                // Generate response text (the same as the PHP file).
+                var responseText = "";
+                for (var i=0; i<80000; i++)
+                    responseText += i + " ";
 
-                FBTestFirebug.testDone("issue1693.DONE");
+                // It takes some time to display huge response so, wait for the last message
+                // saying: a limit has been reached...
+                onResponseDisplayed(function()
+                {
+                    // Compare expected and actuall (displayed) response text.
+                    var text1 = responseText.substr(0, limit);
+                    var text2 = responseBody[0].textContent.substr(0, limit);
+                    FBTest.compare(text1, text2, "The response text must be properly displayed");
+                    FBTestFirebug.testDone("issue1693.DONE");
+                });
             });
+
+            FBTest.click(win.document.getElementById("testButton"));
         });
     });
+}
+
+function onRequestDisplayed(callback)
+{
+    // Create listener for mutation events.
+    var doc = FBTestFirebug.getPanelDocument();
+    var recognizer = new MutationRecognizer(doc.defaultView, "div",
+        {"class": "logRow logRow-spy loaded"});
+
+    // Wait for a XHR log to appear in the Net panel.
+    recognizer.onRecognizeAsync(callback);
+}
+
+function onResponseDisplayed(callback, text)
+{
+    // Create listener for mutation events.
+    var doc = FBTestFirebug.getPanelDocument();
+    var recognizer = new MutationRecognizer(doc.defaultView, "div",
+        {"class": "netInfoResponseSizeLimit"});
+
+    // Wait for a XHR log to appear in the Net panel.
+    recognizer.onRecognizeAsync(callback);
 }
