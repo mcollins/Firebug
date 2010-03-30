9d5bbef0f571eb58b21bcc3ba6d35d3a37d4d964
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-03-30 12:11:03 +0000

    FBTest for Issue 2886: Java Script search field dosnt find some code
    git-svn-id: http://fbug.googlecode.com/svn@6409 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/search/2886/issue2886.js
@@ -0,0 +1,37 @@
+function runTest()
+{
+    FBTest.sysout("issue2886.START");
+
+    FBTest.openNewTab(basePath + "search/2886/issue2886.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableScriptPanel(function(win)
+        {
+            FW.FirebugChrome.selectPanel("script");
+
+            var tests = new FBTest.TaskList();
+            tests.push(doSearch, "(!(this-is-accidental-regexpr))", 28, false);
+            tests.push(doSearch, "keyword\\s+\\d+", 30, true);
+
+            tests.run(function() {
+                FBTest.testDone("issue2886.DONE");
+            });
+        });
+    });
+}
+
+function doSearch(callback, searchString, lineNo, useRegExp)
+{
+    FBTest.progress("Search for " + searchString);
+    FBTest.setPref("searchUseRegularExpression", useRegExp);
+
+    // Execute search.
+    FBTest.searchInScriptPanel(searchString, function(row)
+    {
+        var sourceLine = row.querySelector(".sourceLine");
+        FBTest.compare(lineNo, parseInt(sourceLine.textContent),
+            searchString + " found on line: " + lineNo);
+
+        callback();
+    });
+}
