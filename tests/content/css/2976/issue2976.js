2aa3ec2f7a2571a5663e6778cfb8ec9f296ca467
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-04-13 15:48:03 +0000

    FBTest for Issue 2976: Copy Style Declaration context menu option
    git-svn-id: http://fbug.googlecode.com/svn@6539 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/css/2976/issue2976.js
@@ -0,0 +1,36 @@
+function runTest()
+{
+    FBTest.sysout("issue2976.START");
+    FBTest.openNewTab(basePath + "css/2976/issue2976.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableNetPanel(function(win)
+        {
+            FW.FirebugChrome.selectPanel("html");
+
+            // Search for 'myElement' within the HTML panel, which
+            // automatically expands the tree.
+            FBTest.searchInHtmlPanel("myElement", function(sel)
+            {
+                // Click on the element to make sure it's selected.
+                var nodeLabelBox = FW.FBL.getAncestorByClass(sel.anchorNode, "nodeLabelBox");
+                var nodeTag = nodeLabelBox.querySelector(".nodeTag");
+                FBTest.mouseDown(nodeTag);
+
+                // Reset clipboard content.
+                FBTest.clearClipboard();
+
+                var stylePanel = FW.FirebugChrome.selectSidePanel("css");
+                var node = stylePanel.panelNode.querySelector(".cssSelector");
+                FBTest.executeContextMenuCommand(node, "fbCopyStyleDeclaration", function()
+                {
+                    var cssDecl = FBTest.getClipboardText();
+                    var expected = "background-color: LightYellow;\ncolor: red;\nfont-weight: bold;";
+                    FBTest.compare(expected, cssDecl,
+                        "CSS declaration must be properly copied into the clipboard");
+                    FBTest.testDone("issue2976.DONE");
+                });
+            })
+        });
+    });
+}
