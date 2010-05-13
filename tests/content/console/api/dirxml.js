6fa4ec0e350cc770d9b713cca667190518c8a026
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-05-13 13:12:24 +0000

    Use regular expression when comparing displayed XML (test for console.dirxml).
    git-svn-id: http://fbug.googlecode.com/svn@6733 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/console/api/dirxml.js
+++ tests/content/console/api/dirxml.js
@@ -9,7 +9,7 @@ function runTest()
             var config = {tagName: "div", classes: "logRow logRow-dirxml"};
             FBTest.waitForDisplayedElement("console", config, function(row)
             {
-                var xml = "<div id=\"content\" style=\"display: none;\"><span>a</span><span><span>b</span></span></div>";
+                var xml = /\s*<div\s*id=\"content\"\s*style=\"display:\s*none;\"><span>a<\/span><span><span>b<\/span><\/span><\/div>/;
                 FBTest.compare(xml, row.textContent, "XML must be properly displayed.");
                 FBTest.testDone("console.dirxml.DONE");
             });
