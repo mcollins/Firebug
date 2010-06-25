fa35a220f218294929e8f07ebe40afe1fcffacf3
johnjbarton@johnjbarton.com <johnjbarton@johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-06-25 21:57:29 +0000

    clear the console before test, to ensure that it is scrolledToBottom.
    git-svn-id: http://fbug.googlecode.com/svn@7149 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/console/3078/issue3078.js
+++ tests/content/console/3078/issue3078.js
@@ -4,15 +4,22 @@ function runTest()
     FBTest.openNewTab(basePath + "console/3078/issue3078.html", function(win)
     {
         FBTest.openFirebug();
+        FBTest.enableAllPanels();
+        
+        var panel = FW.FirebugChrome.selectPanel("console");
+        panel.clear();  // ensure that the console starts scrolled to bottom
         
         FBTest.enableConsolePanel(function(win)
         {
-        	var panel = FW.FirebugChrome.selectPanel("console");
-        	FBTest.ok(panel && (panel.name === "console"), "The console panel must be selected");
+            var panel = FW.FirebugChrome.selectPanel("console");
+            FBTest.ok(panel && (panel.name === "console"), "The console panel must be selected");
 
-        	FBTest.ok(FW.FBL.isScrolledToBottom(panel.panelNode),
-                "The panel must be scrolled at the bottom.");
-
+            var scrolled = FW.FBL.isScrolledToBottom(panel.panelNode);
+            if (!scrolled)
+                FBTest.progress("isScrolledToBottom offsetHeight: " + panel.panelNode.offsetHeight +
+                        ", scrollTop: " + panel.panelNode.scrollTop + ", scrollHeight: " + panel.panelNode.scrollHeight);
+                
+            FBTest.ok(scrolled, "The panel must be scrolled at the bottom.");
             FBTest.testDone("issue3078.DONE");
         });
     });
