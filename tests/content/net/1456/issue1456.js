8325fc16a333cdeee6c691f8213e5b27eefc2087
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-06-29 10:31:33 +0000

    Fix test 1456/issue1456 (don't cross chrome and web content scope).
    git-svn-id: http://fbug.googlecode.com/svn@7188 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/net/1456/issue1456.js
+++ tests/content/net/1456/issue1456.js
@@ -2,47 +2,45 @@ window.FBTestTimeout = 13000; // override the default test timeout [ms].
 
 function runTest()
 {
-	var startTime = new Date().getTime();
+    var startTime = new Date().getTime();
+
     FBTest.sysout("issue1456.START");
     var responseText = "$('tb').shake();\n$('tb').value='Some Response';\n";
 
     FBTestFirebug.openNewTab(basePath + "net/1456/issue1456.htm", function(win)
     {
-    	var time = new Date().getTime();
-    	FBTest.progress("opened "+win.location+" at "+ (time - startTime)+"ms");
+        var time = new Date().getTime();
+        FBTest.progress("opened "+win.location+" at "+ (time - startTime)+"ms");
+
         // Open Firebug UI and enable Net panel.
         FBTestFirebug.enableNetPanel(function(win)
         {
-        	var time = new Date().getTime();
-        	FBTest.progress("enabled net panel at "+ (time - startTime)+"ms");
-        	
-        	win.wrappedJSObject.runTest(function doNothing(response)
-        	{
-        		FBTest.progress("Dummy run to workaround Mozilla bug");
-        	});  
+            var time = new Date().getTime();
+            FBTest.progress("enabled net panel at "+ (time - startTime)+"ms");
 
-        	win.wrappedJSObject.runTest(function(response)
+            var options = {tagName: "tr", classes: "netRow category-xhr hasHeaders loaded"};
+            FBTestFirebug.waitForDisplayedElement("net", options, function(row)
             {
-            	var time = new Date().getTime();
-            	FBTest.progress("onResponse at "+ (time - startTime)+"ms");
+                var time = new Date().getTime();
+                FBTest.progress("onResponse at "+ (time - startTime)+"ms");
 
-        		FBTest.sysout("issue1456.onResponse: ", response);
+                FBTest.sysout("issue1456.onResponse: ", row);
 
-                // Expand the test request with params
                 var panelNode = FW.FirebugChrome.selectPanel("net").panelNode;
-                
+
                 var time = new Date().getTime();
                 FBTest.progress("onResponse selectPanel complete"+ (time - startTime)+"ms");
 
-                FBTestFirebug.expandElements(panelNode, "netRow", "category-xhr", "hasHeaders", "loaded");
-                FBTestFirebug.expandElements(panelNode, "netInfoResponseTab");
-                
+                // Expand the test request with params
+                FBTestFirebug.click(row);
+                FBTestFirebug.expandElements(row.nextSibling, "netInfoResponseTab");
+
                 var time = new Date().getTime();
                 FBTest.progress("onResponse expandElements complete"+ (time - startTime)+"ms");
-                
+
                 // The response must be displayed.
-                var responseBody = FW.FBL.getElementByClass(panelNode, "netInfoResponseText",
-                    "netInfoText");
+                var responseBody = FW.FBL.getElementByClass(row.nextSibling,
+                    "netInfoResponseText", "netInfoText");
 
                 FBTest.ok(responseBody, "Response tab must exist.");
                 if (responseBody)
@@ -56,12 +54,14 @@ function runTest()
                     FBTest.compare(responseText, lines.join(""), "Response must match.");
                 }
 
-            	var time = new Date().getTime();
-            	FBTest.progress("done at "+ (time - startTime)+"ms");
+                var time = new Date().getTime();
+                FBTest.progress("done at "+ (time - startTime)+"ms");
 
                 // Finish test
                 FBTestFirebug.testDone("issue1456.DONE");
             });
+
+            FBTest.click(win.document.getElementById("testButton"));
         });
     })
 }
