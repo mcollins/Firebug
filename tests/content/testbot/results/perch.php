b94439d309556e136bae9e1b0d3f550863d3d6f2
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-06-30 17:44:05 +0000

    Use absolute paths.
    git-svn-id: http://fbug.googlecode.com/svn@7211 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/testbot/results/perch.php
+++ tests/content/testbot/results/perch.php
@@ -1,27 +1,27 @@
 <!-- http://brasstacks.mozilla.com/couchdb/_utils/script/ -->
-<script src="couchdb/json2.js"></script>
-<script src="couchdb/jquery.js?1.3.1"></script>
-<script src="couchdb/jquery.couch.js?0.9.0"></script>
+<script src="/tests/content/testbot/results/couchdb/json2.js"></script>
+<script src="/tests/content/testbot/results/couchdb/jquery.js?1.3.1"></script>
+<script src="/tests/content/testbot/results/couchdb/jquery.couch.js?0.9.0"></script>
 
 <!-- Viewer --> 
-<link href="style/main.css" media="screen,projection" type="text/css" rel="stylesheet">
-<link href="style/tableRep.css" media="screen,projection" type="text/css" rel="stylesheet">
-<link href="style/tabView.css" media="screen,projection" type="text/css" rel="stylesheet">
-<link href="style/groupListRep.css" media="screen,projection" type="text/css" rel="stylesheet">
-<link href="style/progressListRep.css" media="screen,projection" type="text/css" rel="stylesheet">
-<link href="style/reps.css" media="screen,projection" type="text/css" rel="stylesheet">
+<link href="/tests/content/testbot/results/style/main.css" media="screen,projection" type="text/css" rel="stylesheet">
+<link href="/tests/content/testbot/results/style/tableRep.css" media="screen,projection" type="text/css" rel="stylesheet">
+<link href="/tests/content/testbot/results/style/tabView.css" media="screen,projection" type="text/css" rel="stylesheet">
+<link href="/tests/content/testbot/results/style/groupListRep.css" media="screen,projection" type="text/css" rel="stylesheet">
+<link href="/tests/content/testbot/results/style/progressListRep.css" media="screen,projection" type="text/css" rel="stylesheet">
+<link href="/tests/content/testbot/results/style/reps.css" media="screen,projection" type="text/css" rel="stylesheet">
 
 <!-- Viewer Javascript -->
-<script type="text/javascript" src="content/domplate.js"></script>
-<script type="text/javascript" src="content/core.js"></script>
-<script type="text/javascript" src="content/lib.js"></script>
-<script type="text/javascript" src="content/reps.js"></script>
-<script type="text/javascript" src="content/tableRep.js"></script>
-<script type="text/javascript" src="content/tabView.js"></script>
-<script type="text/javascript" src="content/groupListRep.js"></script>
-<script type="text/javascript" src="content/progressListRep.js"></script>
-<script type="text/javascript" src="content/firebugDB.js"></script>
-<script type="text/javascript" src="content/main.js"></script>
+<script type="text/javascript" src="/tests/content/testbot/results/content/domplate.js"></script>
+<script type="text/javascript" src="/tests/content/testbot/results/content/core.js"></script>
+<script type="text/javascript" src="/tests/content/testbot/results/content/lib.js"></script>
+<script type="text/javascript" src="/tests/content/testbot/results/content/reps.js"></script>
+<script type="text/javascript" src="/tests/content/testbot/results/content/tableRep.js"></script>
+<script type="text/javascript" src="/tests/content/testbot/results/content/tabView.js"></script>
+<script type="text/javascript" src="/tests/content/testbot/results/content/groupListRep.js"></script>
+<script type="text/javascript" src="/tests/content/testbot/results/content/progressListRep.js"></script>
+<script type="text/javascript" src="/tests/content/testbot/results/content/firebugDB.js"></script>
+<script type="text/javascript" src="/tests/content/testbot/results/content/main.js"></script>
 
 <br/>
 <div id="content"></div>
