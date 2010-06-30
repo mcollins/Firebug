9b28235b0b817aeedc1c98897daae7b1ce4c02c6
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-06-30 17:09:52 +0000

    Missing file
    git-svn-id: http://fbug.googlecode.com/svn@7210 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/testbot/results/perch.php
@@ -0,0 +1,27 @@
+<!-- http://brasstacks.mozilla.com/couchdb/_utils/script/ -->
+<script src="couchdb/json2.js"></script>
+<script src="couchdb/jquery.js?1.3.1"></script>
+<script src="couchdb/jquery.couch.js?0.9.0"></script>
+
+<!-- Viewer --> 
+<link href="style/main.css" media="screen,projection" type="text/css" rel="stylesheet">
+<link href="style/tableRep.css" media="screen,projection" type="text/css" rel="stylesheet">
+<link href="style/tabView.css" media="screen,projection" type="text/css" rel="stylesheet">
+<link href="style/groupListRep.css" media="screen,projection" type="text/css" rel="stylesheet">
+<link href="style/progressListRep.css" media="screen,projection" type="text/css" rel="stylesheet">
+<link href="style/reps.css" media="screen,projection" type="text/css" rel="stylesheet">
+
+<!-- Viewer Javascript -->
+<script type="text/javascript" src="content/domplate.js"></script>
+<script type="text/javascript" src="content/core.js"></script>
+<script type="text/javascript" src="content/lib.js"></script>
+<script type="text/javascript" src="content/reps.js"></script>
+<script type="text/javascript" src="content/tableRep.js"></script>
+<script type="text/javascript" src="content/tabView.js"></script>
+<script type="text/javascript" src="content/groupListRep.js"></script>
+<script type="text/javascript" src="content/progressListRep.js"></script>
+<script type="text/javascript" src="content/firebugDB.js"></script>
+<script type="text/javascript" src="content/main.js"></script>
+
+<br/>
+<div id="content"></div>
