8560aa4f1a1e043527eecca34679ff83963523a8
johnjbarton@johnjbarton.com <johnjbarton@johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-07-31 00:57:13 +0000

    notify observers when tests are started
    git-svn-id: http://fbug.googlecode.com/svn@7453 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ fbtest/branches/fbtest1.7/content/notify.js
@@ -0,0 +1,27 @@
+/* See license.txt for terms of usage */
+
+FBTestApp.ns(function() { with (FBL) {
+
+// ************************************************************************************************
+// Connects FBTest to other modules via global events
+
+var rebroadcaster = 
+{
+		onTestStart: function(aTest)
+		{
+			if (!this.observerService)
+				Components.utils.import("resource://firebug/observer-service.js", this);
+			
+			this.observerService.notifyObservers(this, "fbtest-start-case", aTest);			
+		},
+		// *******************************************************************************
+		replay: function(aTest)
+		{
+			FBTestApp.TestRunner.runTest(aTest);
+		},
+}
+
+FBTestApp.TestRunner.addListener(rebroadcaster);
+
+// ************************************************************************************************
+}});
