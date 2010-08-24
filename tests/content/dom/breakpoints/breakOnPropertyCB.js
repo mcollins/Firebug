51a1574c108a4647cc8d29cf9797f6a76ca946c7
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-08-24 13:40:32 +0000

    Fix BON tests
    git-svn-id: http://fbug.googlecode.com/svn@7639 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/dom/breakpoints/breakOnPropertyCB.js
@@ -0,0 +1,96 @@
+function runTest()
+{
+    FBTest.sysout("dom.breakpoints.CB; START");
+    FBTest.setPref("service.filterSystemURLs", false);
+    
+    FBTestFirebug.openNewTab(basePath + "dom/breakpoints/breakOnProperty.html", function(win)
+    {
+        FBTestFirebug.openFirebug();
+        FBTestFirebug.enableAllPanels();
+
+        FBTestFirebug.selectPanel("dom");
+
+        // Reload window to activate debugger and run breakOnTest.
+        FBTestFirebug.reload(function(win)
+        {
+            var panel = FBTestFirebug.getPanel("dom");
+            var row = getPropertyRow(panel, "anObject");
+            onPropertyDisplayed(row, "anObject", function(row)
+            {
+                // Wait till _testProperty row in the UI is available. This row displays
+                // the _testProperty and we need to created a breakpoint on it.
+                onPropertyDisplayed(null, "_testProperty", function(element)
+                {
+                    // Set breakpoint.
+                    panel.breakOnProperty(element);
+
+                    var doc = element.ownerDocument;
+                    var testSuite = [];
+                    testSuite.push(function(callback) {
+                        FBTest.progress("4 " + win);
+                        breakOnMutation(win, "changeProperty", 45, callback);
+                    });
+                    testSuite.push(function(callback) {
+                        FBTest.click(win.document.getElementById("removeProperty"));
+                        callback();
+                    });
+                    testSuite.push(function(callback) {
+                        breakOnMutation(win, "addProperty", 40, callback);
+                    });
+                    testSuite.push(function(callback) {
+                        breakOnMutation(win, "changeProperty", 45, callback);
+                    });
+
+                    FBTestFirebug.runTestSuite(testSuite, function() {
+                        FBTestFirebug.testDone("dom.breakpoints.CB; DONE");
+                    });
+                });
+
+                // Click to expand object's properties.
+                var label = row.getElementsByClassName("memberLabel").item(0);
+                FBTest.click(label);
+            });
+        });
+    });
+}
+
+function onPropertyDisplayed(element, propName, callback)
+{
+    if (element)
+        return callback(element);
+
+    var panel = FBTestFirebug.getPanel("dom");
+    var recognizer = new MutationRecognizer(panel.document.defaultView,
+        "Text", {}, propName);
+    recognizer.onRecognizeAsync(callback);
+}
+
+function breakOnMutation(win, buttonId, lineNo, callback)
+{
+    FBTest.progress("breakOnMutation.CB; " + buttonId);
+
+    FBTestFirebug.selectPanel("dom");
+
+    var chrome = FW.Firebug.chrome;
+    FBTestFirebug.waitForBreakInDebugger(chrome, lineNo, false, function(sourceRow)
+    {
+        FBTestFirebug.clickContinueButton(chrome);
+        FBTest.progress("The continue button is pushed");
+        callback();
+    });
+
+    FBTest.click(win.document.getElementById(buttonId));
+}
+
+function getPropertyRow(panel, propName)
+{
+    var rows = FW.FBL.getElementsByClass(panel.panelNode, "memberRow", "userRow");
+    for (var i=0; i<rows.length; i++)
+    {
+        var row = rows[i];
+        var label = FW.FBL.getElementByClass(row, "memberLabel", "userLabel");
+        if (label.textContent == propName)
+            return row;
+    }
+    return null;
+}
