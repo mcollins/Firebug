0260687cf4377b9d195bab6f2b8bafc0081015a7
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-08-26 11:43:42 +0000

    Yet one improvement of the script/1483 test, now it's perfect.
    git-svn-id: http://fbug.googlecode.com/svn@7668 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/script/1483/driveIssue1483-1.6.js
+++ tests/content/script/1483/driveIssue1483-1.6.js
@@ -1,8 +1,8 @@
-var fileName = "index.js";
-var lineNo = 5;
-
 function runTest()
 {
+    var fileName = "index.js";
+    var lineNo = 5;
+
     FBTest.sysout("issue1483.START");
     FBTest.openNewTab(basePath + "script/1483/issue1483.html", function(win)
     {
@@ -17,15 +17,18 @@ function runTest()
             FBTest.progress("issue1483.script panel enabled");
 
             // Set breakpoint in index.js file at line 5
-            FBTest.setBreakpoint(null, fileName, 5, function()
+            FBTest.setBreakpoint(null, fileName, lineNo, function()
             {
                 FBTest.progress("issue1483.a breakpoint is set");
 
+                var hit = false; // a flag indicating that a break happened.
                 var chrome = FW.Firebug.chrome;
+
                 FBTest.waitForBreakInDebugger(chrome, lineNo, true, function()
                 {
+                    hit = true;
                     FBTest.progress("issue1483.break on the breakpoint");
-                    removeBreakpoint(chrome, fileName, 5, function()
+                    removeBreakpoint(chrome, fileName, lineNo, function()
                     {
                         FBTest.clickContinueButton(chrome);
                         FBTest.progress("issue1483.the continue button is pused");
@@ -36,6 +39,7 @@ function runTest()
                 FBTest.reload(function()
                 {
                     FBTest.progress("issue1483.page reloaded");
+                    FBTest.ok(hit, "The break happened");
 
                     // Check the Console panel
                     var panelNode = FBTest.selectPanel("console").panelNode;
