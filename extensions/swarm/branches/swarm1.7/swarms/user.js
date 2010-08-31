aa9b952c53cc82ef5fb1fbf7abb639545d3f4505
johnjbarton@johnjbarton.com <johnjbarton@johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-08-31 21:08:01 +0000

    git-svn-id: http://fbug.googlecode.com/svn@7710 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ extensions/swarm/branches/swarm1.7/swarms/user.js
@@ -0,0 +1,4 @@
+user_pref("xpinstall.whitelist.required", "false");
+user_pref("browser.rights.3.shown", true);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("xpinstall.whitelist.add", "getfirebug.com");
