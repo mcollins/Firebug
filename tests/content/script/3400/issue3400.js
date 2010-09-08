f1854c8aad16f589be4ff55e82b02bc6395114b7
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-09-08 07:53:14 +0000

    FBTest for issue 3400: Debugger buttons not visible when long stack trace + update test list 1.7
    git-svn-id: http://fbug.googlecode.com/svn@7765 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/script/3400/issue3400.js
@@ -0,0 +1,31 @@
+function runTest()
+{
+    FBTest.sysout("issue3400.START");
+    FBTest.openNewTab(basePath + "script/3400/issue3400.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableScriptPanel(function(win)
+        {
+            var panel = FW.FirebugChrome.selectPanel("script");
+
+            var chrome = FW.Firebug.chrome;
+            FBTest.waitForBreakInDebugger(chrome, 29, false, function(row)
+            {
+                var doc = chrome.window.document;
+                var button = doc.getElementById("fbStepOutButton");
+                var toolbar = doc.getElementById("fbToolbar");
+                var rect = button.getClientRects()[0];
+
+                FBTest.progress("script panel toolbar width: " + toolbar.clientWidth +
+                    ", step-out button right side: " + rect.right);
+                FBTest.ok(toolbar.clientWidth > rect.right, "Debugger buttons must be visible");
+
+                // Resume debugger and finish the test.
+                FBTest.clickContinueButton();
+                FBTest.testDone("issue3400.DONE");
+            });
+
+            FBTest.click(win.document.getElementById("testButton"));
+        });
+    });
+}
