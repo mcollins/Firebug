cb21cea4b6fe386c608bd3c9971136fc965aafb6
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-09-15 15:47:14 +0000

    FBTest for 3394 (completion)
    git-svn-id: http://fbug.googlecode.com/svn@7811 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/console/completion/3394/issue3394.js
@@ -0,0 +1,41 @@
+function runTest()
+{
+    FBTest.sysout("issue3394.START");
+    FBTest.openNewTab(basePath + "console/completion/3394/issue3394.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableConsolePanel(function(win)
+        {
+            var panel = FW.FirebugChrome.selectPanel("console");
+
+            typeCommand("loc");
+
+            FBTest.synthesizeKey("VK_TAB", win); // 9 == tab
+
+            var doc = FW.FirebugChrome.window.document;
+            var cmdLine = doc.getElementById("fbCommandLine");
+            FBTest.compare("locationbar", cmdLine.value,
+                "The autocomplete must produce: locationbar");
+
+            FBTest.testDone("issue3394.DONE");
+        });
+    });
+}
+
+// ************************************************************************************************
+// xxxHonza: These should be polished and moved into FBTest namespace.
+
+function typeCommand(string)
+{
+    var doc = FW.FirebugChrome.window.document;
+    var cmdLine = doc.getElementById("fbCommandLine");
+    var panelBar1 = doc.getElementById("fbPanelBar1");
+    var win = panelBar1.browser.contentWindow;
+
+    FW.FirebugChrome.window.focus();
+    panelBar1.browser.contentWindow.focus();
+    FBTest.focus(cmdLine);
+
+    for (var i=0; i<string.length; ++i)
+        FBTest.synthesizeKey(string.charAt(i), win);
+}
