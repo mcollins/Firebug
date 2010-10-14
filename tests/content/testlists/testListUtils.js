d3ff6c568897f67c8d4f1ab76194ef3e79b3c950
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-10-14 09:17:08 +0000

    Online (generated) test list should point to test case pages not to drivers.
    git-svn-id: http://fbug.googlecode.com/svn@8033 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/testlists/testListUtils.js
+++ tests/content/testlists/testListUtils.js
@@ -1,11 +1,13 @@
-function getDriverBaseURI()
-{
-    var parts = window.location.toString().split('/');
-    parts.pop(); // remove file name
-    parts.pop(); // remove /testLists/
-    return parts.join('/') + "/";
-}
+/* See license.txt for terms of usage */
 
+// **********************************************************************************************//
+// Constants
+
+/**
+ * Convert list of test (JS array) into a nice HTML markup.
+ * xxxHonza: I'd like to use a domplate module (requirejs) + a nice template
+ * that renders the JS array with tests.
+ */
 window.addEventListener("load", function showPage()
 {
     // If the file is loaded into the Firebug test console don't 
@@ -26,15 +28,44 @@ window.addEventListener("load", function showPage()
     for(var i = 0; i < testList.length; i++)
     {
         var testCase = testList[i];
+        var ul;
+
         if (testCase.group != currentGroup)
         {
             currentGroup = testCase.group;
-            var h3 = document.createElement('h3');
-            h3.innerHTML = currentGroup.charAt(0).toUpperCase() + currentGroup.substr(1).toLowerCase();
+            var h3 = document.createElement("h3");
+
+            // Capitalize the first letter.
+            h3.innerHTML = currentGroup.charAt(0).toUpperCase() +
+                currentGroup.substr(1).toLowerCase();
+
+            var anchor = document.createElement("a");
+            anchor.setAttribute("name", currentGroup);
+            cases.appendChild(anchor);
             cases.appendChild(h3);
+
+            ul = document.createElement("ul");
+            cases.appendChild(ul);
         }
-        var entry = document.createElement('div');
-        entry.innerHTML="<a href="+driverBaseURI+testCase.uri+">"+testCase.desc+"</a>";
-        cases.appendChild(entry);
+
+        var entry = document.createElement("li");
+        var uri = testCase.testPage ? testCase.testPage : testCase.uri;
+        entry.innerHTML += "<a href=" + driverBaseURI + uri + ">" +
+            testCase.desc + "</a>";
+
+        if (!testCase.testPage)
+            entry.innerHTML += "<span class='noTestCasePage'>(driver only)</span>";
+
+        ul.appendChild(entry);
     }
 }, true);
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+function getDriverBaseURI()
+{
+    var parts = window.location.toString().split('/');
+    parts.pop(); // remove file name
+    parts.pop(); // remove /testLists/
+    return parts.join('/') + "/";
+}
