16e6ccb1d85a1655b571be571769cd04af6e80aa
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-11-11 18:08:03 +0000

    Adapt test to better APIs.
    git-svn-id: http://fbug.googlecode.com/svn@8408 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/dom/3597/issue3597.js
+++ tests/content/dom/3597/issue3597.js
@@ -4,18 +4,19 @@ function runTest()
     FBTest.openNewTab(basePath + "dom/3597/issue3597.html", function(win)
     {
         FBTest.openFirebug();
+        FBTest.selectPanel("dom");
 
         expandProperty("_testString", "childObj", function()
         {
             expandProperty("childObj", "lastItem", function()
             {
                 FBTest.click(win.document.getElementById("testButton"));
-                panel.rebuild(true);
+                FBTest.refreshDOMPanel();
 
                 // Wait till the _testString is displayed again.
                 FBTest.waitForDOMProperty("_testString", function(row)
                 {
-                    var row = getPropertyRow(panel, "_testString");
+                    var row = FBTest.getDOMPropertyRow(null, "_testString");
 
                     // The _testString must have 'string' type now.
                     FBTest.compare(
@@ -30,11 +31,11 @@ function runTest()
 }
 
 //xxxHonza: should be part of FBTest namespace. See also dom/2772
-function expandProperty(panel, propName, lastChild, callback)
+function expandProperty(propName, lastChild, callback)
 {
     FBTest.waitForDOMProperty(lastChild, callback);
 
-    var row = getPropertyRow(panel, propName);
+    var row = FBTest.getDOMPropertyRow(null, propName);
     var propLabel = row.querySelector(".memberLabel.userLabel");
     FBTest.click(propLabel);
 }
