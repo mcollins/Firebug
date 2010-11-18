0c1d24db7a5caa4122f858da8dad8bfb6229b1c4
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-11-18 18:22:49 +0000

    Improve test for issue 3652 (CSS inline editor completion) for testing also up and down arrows in order to cover also 3671)
    git-svn-id: http://fbug.googlecode.com/svn@8480 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/css/3652/issue3652.js
+++ tests/content/css/3652/issue3652.js
@@ -15,10 +15,30 @@ function runTest()
             // Click the CSS value to open the inline editor.
             FBTest.synthesizeMouse(values[0]);
 
-            // Type 'r' and verify auto competion.
+            // Type 'r' and verify auto completion.
             var editor = panel.panelNode.querySelector(".textEditorInner");
             FBTest.sendChar("r", editor);
             FBTest.compare("red", editor.value, "Must be autocompleted to red.");
+
+            // Testing up and down arrows covers issue 3671
+            // Type 'arrow-up' and verify completion (should be the previouis
+            // color startin with 'r').
+            FBTest.sendKey("UP", editor);
+            FBTest.compare("royalBlue", editor.value, "Must be autocompleted to royalBlue.");
+
+            // Type 'arrow-down' and verify completion.
+            FBTest.sendKey("DOWN", editor);
+            FBTest.compare("red", editor.value, "Must be autocompleted again to red.");
+
+            // Type 'home' to move the cursor at the begginging and cancel the selection.
+            // Consequently type 'arrow-up' to get the (global) previous color.
+            FBTest.sendKey("HOME", editor);
+            FBTest.sendKey("UP", editor);
+            FBTest.compare("Purple", editor.value, "Must be autocompleted to Purple.");
+
+            // And again go back to 'Red' (now with capital R)
+            FBTest.sendKey("DOWN", editor);
+            FBTest.compare("Red", editor.value, "Must be autocompleted to red.");
 
             FBTest.testDone("issue3652.DONE");
         });
