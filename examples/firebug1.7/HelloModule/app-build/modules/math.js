3800101a574bc2ec0ab7ae07dbb65ab70f88cf8d
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-12-16 00:00:20 +0000

    Optimization build for HelloModule example.
    git-svn-id: http://fbug.googlecode.com/svn@8753 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ examples/firebug1.7/HelloModule/app-build/modules/math.js
@@ -0,0 +1,12 @@
+/* See license.txt for terms of usage */
+
+define(function(require, exports, module) {
+
+// ************************************************************************************************
+// Module Implementation
+
+exports.subtract = require("subtract");
+exports.add = require("add");
+
+// ************************************************************************************************
+});
---
+++ examples/firebug1.7/HelloModule/app/modules/firebug-trace.js
@@ -0,0 +1,37 @@
+/* See license.txt for terms of usage */
+
+define(function(require, exports, module) {
+
+// ********************************************************************************************* //
+// Module Implementation
+
+// We need to use the console object in case of the web page that doesn't have proper
+// privileges to use Components.utils
+
+var FBTrace =
+{
+    sysout: function()
+    {
+        if (typeof(console.log) == "function")
+            console.log.apply(console, arguments);
+    }
+};
+
+try
+{
+    var FirebugTrace = {};
+    Components.utils["import"]("resource://firebug/firebug-trace-service.js", FirebugTrace);
+    FBTrace = FirebugTrace.traceConsoleService.getTracer("extensions.firebug");
+}
+catch (e)
+{
+}
+
+
+// ********************************************************************************************* //
+// Exported Symbols
+
+exports.FBTrace = FBTrace;
+
+// ********************************************************************************************* //
+});
