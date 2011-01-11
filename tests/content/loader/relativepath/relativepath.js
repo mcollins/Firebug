0112a81f526cf59c3b44c588a2128904ad8396b6
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-01-11 11:26:06 +0000

    FBTest for module loader.
    git-svn-id: http://fbug.googlecode.com/svn@8879 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/loader/relativepath/relativepath.js
@@ -0,0 +1,21 @@
+function runTest()
+{
+    FBTest.sysout("relativepath.START;");
+    FBTest.progress(baseLocalPath);
+
+    Components.utils["import"]("resource://hellomodule/ModuleLoader.js");
+
+    // Compute relative path and conctruct module loader.
+    var baseUrl = baseLocalPath + "loader/relativepath/";
+    var require = (new ModuleLoader(null, {
+        context: baseUrl,
+        baseUrl: baseUrl}
+    )).loadDepsThenCallback;
+
+    require(["add.js", "subtract.js"], function(AddModule, SubtractModule)
+    {
+        FBTest.compare(3, AddModule.add(1, 2), "The add module must be properly loaded");
+        FBTest.compare(2, SubtractModule.subtract(3, 1), "The subtract module must be properly loaded");
+        FBTest.testDone("relativepath.DONE");
+    });
+}
