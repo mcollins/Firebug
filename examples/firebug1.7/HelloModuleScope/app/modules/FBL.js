3af4a34f133c93e08bfdd20b3edb7a1dcc655efa
pedrosimonetti@gmail.com <pedrosimonetti@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-01-20 23:48:19 +0000

    Better way to define "cluster-modules" without using require().
    git-svn-id: http://fbug.googlecode.com/svn@9003 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- examples/firebug1.7/HelloModuleScope/app/modules/FBL.js
+++ examples/firebug1.7/HelloModuleScope/app/modules/FBL.js
@@ -1,18 +1,16 @@
 /* See license.txt for terms of usage */
 
-define(function(require, exports, object) {
-
+define("FBL",
+[
+    "exports", // comment this line and it will throw "FBL is undefined" error
+    "Core/Base",
+    "Core/FBTrace",
+    "Core/CSS",
+    "Core/DOM",
+    "Core/Event"
+],
+function(exports, FBL){
 // ********************************************************************************************* //
-// Lib components
-
-var FBL = require("Core/Base");
-require("Core/FBTrace");
-require("Core/CSS");
-require("Core/DOM");
-require("Core/Event");
-
-// ********************************************************************************************* //
-// public
 
 return FBL;
 
