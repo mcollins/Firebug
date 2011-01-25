7d624a21ce1ffe90e8bf815043ebb333a8ef55e6
johnjbarton@johnjbarton.com <johnjbarton@johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-01-25 19:22:06 +0000

    [modules] still dont understand what is going on
    git-svn-id: http://fbug.googlecode.com/svn@9052 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- extensions/moduleloader/branches/moduleloader0.1/bootstrap.js
+++ extensions/moduleloader/branches/moduleloader0.1/bootstrap.js
@@ -1,25 +1,35 @@
 var Cc = Components.classes;
 var Ci = Components.interfaces;
 var Cu = Components.utils;
-try
-{
-    Components.utils.import("resource://firebug/firebug-trace-service.js");
-    Components.utils.reportError("bootstrap loaded "+FBTrace);
-    }
-catch (exc)
-{
-     Cu.reportError("bootstrap 0 ERROR "+exc);
-}
-//Cu.import("resource://moduleloader/moduleLoader.js");
+//
 /*
  * bootstrap.js API
  * https://wiki.mozilla.org/Extension_Manager:Bootstrapped_Extensions
 */
 function startup(aData, aReason) {
 
+    try
+    {
+        Components.utils.import("resource://firebug/require.js");
+        Components.utils.reportError("bootstrap loaded require on startup");
+        }
+    catch (exc)
+    {
+         Cu.reportError("bootstrap 0 ERROR "+exc);
+         debugger;
+    }
 
     try
     {
+        Components.utils.import("resource://gre/modules/Services.jsm");
+         let resource = Services.io.getProtocolHandler("resource").QueryInterface(Ci.nsIResProtocolHandler);
+          let alias = Services.io.newFileURI(aData.installPath);
+          if (!aData.installPath.isDirectory())
+            alias = Services.io.newURI("jar:" + alias.spec + "!/", null, null);
+          resource.setSubstitution("moduleloader", alias);
+          Cu.reportError("bootstrap aData.installPath "+aData.installPath);
+          Cu.reportError("bootstrap alias "+alias.spec);
+          Cu.import("resource://moduleloader/modules/moduleLoader.js");
         Cu.reportError("bootstrap loaded "+ModuleLoader);
     }
     catch(exc)
