50e0b6bc2e8ea476a2dc079b0e8fa7d7385ca428
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-02-07 20:12:53 +0000

    Issue 4031: fix FBTest html/onMutate
    git-svn-id: http://fbug.googlecode.com/svn@9244 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/html/onMutate.js
+++ tests/content/html/onMutate.js
@@ -2,183 +2,153 @@ function runTest()
 {
     FBTest.sysout("html.breakpoints; START");
 
-    FBTestFirebug.openNewTab(basePath + "html/onMutate.html", function(win)
+    FBTest.openNewTab(basePath + "html/onMutate.html", function(win)
     {
-        FBTestFirebug.openFirebug();
-        FBTestFirebug.setPref("highlightMutations", true);
-        FBTestFirebug.setPref("scrollToMutations", true);
-
-        function onMutateText(id, inline, callback) {
-          FBTest.progress("onMutateTest " + id);
-          var mutateId = win.document.getElementById(id);
-
-          var chrome = FW.Firebug.chrome;
-          waitForHtmlMutation(chrome, inline ? "span" : "div", inline ? mutateId : mutateId.firstChild, callback);
-
-          mutateId.firstChild.appendData("test");
-        }
-        function onMutateAttr(id, attr, value, callback) {
-          FBTest.progress("onMutateAttr " + id);
-          var mutateId = win.document.getElementById(id);
-
-          var chrome = FW.Firebug.chrome;
-          waitForHtmlMutation(chrome, value ? "span" : "div", mutateId.firstChild, callback);
-
-          if (value) {
-            mutateId.firstChild.setAttribute(attr, value);
-          } else {
-            mutateId.firstChild.removeAttribute(attr);
-          }
-        }
-        function onMutateNode(id, callback) {
-          FBTest.progress("onMutateNode " + id);
-          var mutateId = win.document.getElementById(id);
-          var button = win.document.createElement("button");
-          var chrome = FW.Firebug.chrome;
-
-          function parentCallback() {
-            waitForHtmlMutation(chrome, "div", button, callback);
-          }
-          waitForHtmlMutation(chrome, "div", mutateId, parentCallback);
-
-          mutateId.appendChild(button);
-        }
-        function onRemoveNode(id, index, callback) {
-          FBTest.progress("onRemoveNode " + id);
-
-          var mutateId = win.document.getElementById(id);
-
-          var chrome = FW.Firebug.chrome;
-          waitForHtmlMutation(chrome, "div", mutateId, callback);
-
-          mutateId.removeChild(mutateId.childNodes[index]);
-        }
-        function onMutateRemovedRace(id, callback) {
-          FBTest.progress("onMutateRemovedRace " + id);
-          var mutateId = win.document.getElementById(id);
-          var button = win.document.createElement("button");
-
-          var chrome = FW.Firebug.chrome;
-          waitForHtmlMutation(chrome, "div", mutateId, callback);
-
-          mutateId.appendChild(button);
-          mutateId.lastChild.setAttribute("title", "test");
-
-          mutateId.firstChild.appendData("test");
-
-          mutateId.innerHTML = "&nbsp;";
-        }
+        FBTest.openFirebug();
+        FBTest.setPref("highlightMutations", true);
+        FBTest.setPref("scrollToMutations", true);
 
         // A suite of asynchronous tests.
-        var testSuite = [];
-
-        testSuite.push(function(callback) {
-          onMutateText("mutateText", false, callback);
-        });
-        testSuite.push(function(callback) {
-          onMutateText("mutateTextInline", true, callback);
-        });
-
-        testSuite.push(function(callback) {
-          onMutateNode("mutateNode", callback);
-        });
-        testSuite.push(function(callback) {
-          onMutateNode("mutateNodeText", callback);
-        });
-        testSuite.push(function(callback) {
-          onMutateNode("mutateNodeEmpty", callback);
-        });
-
-        testSuite.push(function(callback) {
-          onRemoveNode("removeNode", 1, callback);
-        });
-        testSuite.push(function(callback) {
-          onRemoveNode("removeNodeText", 0, callback);
-        });
-        testSuite.push(function(callback) {
-          onRemoveNode("removeNodeEmpty", 0, callback);
-        });
-
-        testSuite.push(function(callback) {
-            onMutateAttr("mutateAttrNew", "title", "boo", callback);
-        });
-        testSuite.push(function(callback) {
-            onMutateAttr("mutateAttrSet", "title", "boo", callback);
-        });
-        testSuite.push(function(callback) {
-            onMutateAttr("mutateAttrRemove", "title", undefined, callback);
-        });
-        testSuite.push(function(callback) {
-            onMutateRemovedRace("mutateRemovedRace", callback);
-        });
+        var testSuite = new FBTest.TaskList();
+        testSuite.push(onMutateText, win, "mutateText", false);
+        testSuite.push(onMutateText, win, "mutateTextInline", true);
+        testSuite.push(onMutateNode, win, "mutateNode");
+        testSuite.push(onMutateNode, win, "mutateNodeText");
+        testSuite.push(onMutateNode, win, "mutateNodeEmpty");
+        testSuite.push(onRemoveNode, win, "removeNode", 1);
+        testSuite.push(onRemoveNode, win, "removeNodeText", 0);
+        testSuite.push(onRemoveNode, win, "removeNodeEmpty", 0);
+        testSuite.push(onMutateAttr, win, "mutateAttrNew", "title", "boo");
+        testSuite.push(onMutateAttr, win, "mutateAttrSet", "title", "boo");
+        testSuite.push(onMutateAttr, win, "mutateAttrRemove", "title", undefined);
+        testSuite.push(onMutateRemovedRace, win, "mutateRemovedRace");
 
         // Reload window to activate debugger and run all tests.
-        FBTestFirebug.reload(function(win) {
-            FBTestFirebug.runTestSuite(testSuite, function() {
-                FBTestFirebug.testDone("html.onMutate; DONE");
+        FBTest.reload(function(win)
+        {
+            testSuite.run(function() {
+                FBTest.testDone("html.onMutate; DONE");
             });
         })
     });
 }
 
+function onMutateText(callback, win, id, inline)
+{
+    FBTest.progress("onMutateTest " + id);
 
-/**
- * Registers handler for break in Debugger.
- * @param {Object} chrome Current Firebug's chrome object (e.g. FW.Firebug.chrome)
- * @param {Number} lineNo Expected source line number where the break should happen.
- * @param {Object} breakpoint Set to true if breakpoint should be displayed in the UI.
- * @param {Object} callback Handeler that should be called when break happens.
- */
+    var mutateId = win.document.getElementById(id);
+    waitForHtmlMutation(null, inline ? "span" : "div", mutateId, callback);
+
+    FBTest.click(win.document.getElementById(id + "Button"));
+}
+
+function onMutateAttr(callback, win, id, attr, value)
+{
+    FBTest.progress("onMutateAttr " + id);
+
+    var mutateId = win.document.getElementById(id);
+    waitForHtmlMutation(null, value ? "span" : "div",
+        value ? mutateId.firstChild : mutateId, callback);
+
+    if (value)
+        mutateId.firstChild.setAttribute(attr, value);
+    else
+        mutateId.firstChild.removeAttribute(attr);
+
+    FBTest.click(win.document.getElementById(id + "Button"));
+}
+
+function onMutateNode(callback, win, id)
+{
+    FBTest.progress("onMutateNode " + id);
+
+    var counter = 0;
+    function done()
+    {
+        if (++counter == 2)
+            callback();
+    }
+
+    var mutateId = win.document.getElementById(id);
+    waitForHtmlMutation(null, "div", mutateId, done);
+    waitForHtmlMutation(null, "div", null, done);
+
+    FBTest.click(win.document.getElementById(id + "Button"));
+}
+
+function onRemoveNode(callback, win, id, index)
+{
+    FBTest.progress("onRemoveNode " + id);
+
+    var mutateId = win.document.getElementById(id);
+    waitForHtmlMutation(null, "div", mutateId.parentNode, callback);
+
+    FBTest.click(win.document.getElementById(id + "Button"));
+}
+
+function onMutateRemovedRace(callback, win, id)
+{
+    FBTest.progress("onMutateRemovedRace " + id);
+
+    var counter = 0;
+    function done()
+    {
+        if (++counter == 3)
+            callback();
+    }
+
+    var mutateId = win.document.getElementById(id);
+    waitForHtmlMutation(null, "div", mutateId.parentNode, done);
+    waitForHtmlMutation(null, "div", null, done);
+    waitForHtmlMutation(null, "div", null, done);
+
+    FBTest.click(win.document.getElementById(id + "Button"));
+}
+
 function waitForHtmlMutation(chrome, tagName, object, callback)
 {
-    FBTest.progress("fbTestFirebug.waitForBreakInDebugger in chrome.window" + chrome.window.location);
+    FBTest_waitForHtmlMutation(chrome, tagName, function(node)
+    {
+        if (object)
+        {
+            var repObj = FW.Firebug.getRepObject(node);
+            if (!repObj)
+                repObj = FW.Firebug.getRepObject(node.getElementsByClassName("repTarget")[0]);
 
-    var panel = FBTestFirebug.selectPanel("html");
-    panel.select(undefined, true);
+            FBTest.compare(object.parentNode.innerHTML, repObj.parentNode.innerHTML, "Element matches");
+            FBTest.compare(object.innerHTML, repObj.innerHTML, "Content matches");
+            FBTest.compare(object, repObj, "Objects matches");
+        }
 
-    // Get document of Firebug's panel.html
-    var panel = chrome.getSelectedPanel();
-    var doc = panel.panelNode.ownerDocument;
+        callback(node);
+    });
+}
 
-    // Complete attributes that must be set on sourceRow element.
+// ********************************************************************************************* //
+
+// xxxHonza: remove as soon as it's part of FBTest 1.7b11
+function FBTest_waitForHtmlMutation(chrome, tagName, callback)
+{
+    if (!chrome)
+        chrome = FW.Firebug.chrome;
+
+    var panel = FBTest.selectPanel("html");
+
+    var doc = FBTest.getPanelDocument();
+    var view = doc.defaultView;
     var attributes = {"class": "mutated"};
 
-    // Wait for mutation event
-    var lookBP = new MutationRecognizer(doc.defaultView, tagName, attributes);
-    lookBP.onRecognize(function onMutate(node)
+    // Wait for mutation event. The HTML panel will set "mutate" class on the
+    // corresponding element.
+    var mutated = new MutationRecognizer(view, tagName, attributes);
+    mutated.onRecognizeAsync(function onMutate(node)
     {
-        var panel = chrome.getSelectedPanel();
-
-        var repObj = FW.Firebug.getRepObject(node)
-            || FW.Firebug.getRepObject(node.getElementsByClassName("repTarget")[0]);
-        FBTest.ok(FW.FBL.hasClass(node, "mutated"), "Mutated class: " + node.getAttribute("class") + " " + node.tagName);
-        FBTest.compare(object, repObj, "Object matches")
-
-        try
+        // Now wait till the HTML panel unhighlight the element (removes the mutate class)
+        var unmutated = new MutationRecognizer(view, tagName, null, null, attributes);
+        unmutated.onRecognizeAsync(function onUnMutate(node)
         {
             callback(node);
-        }
-        catch(exc)
-        {
-            FBTest.sysout("listenForHtmlMutation callback FAILS "+exc, exc);
-        }
+        });
     });
-
-    FBTest.sysout("fbTestFirebug.waitForHtmlMutation recognizing ", lookBP);
-}
-function breakOnMutation(win, type, buttonId, lineNo, callback)
-{
-    var content = win.document.getElementById("content");
-    var context = chrome.window.FirebugContext;
-
-    waitForBreakInDebugger(chrome, lineNo, false, function(sourceRow)
-    {
-        FBTest.sysout("html.breakpoints; " + buttonId);
-        FBTestFirebug.clickContinueButton(chrome);
-        FBTest.progress("The continue button is pushed");
-        callback();
-    });
-
-    FBTest.click(win.document.getElementById(buttonId));
-    FBTest.sysout("html.breakpoints; " + buttonId + " button clicked");
 }
