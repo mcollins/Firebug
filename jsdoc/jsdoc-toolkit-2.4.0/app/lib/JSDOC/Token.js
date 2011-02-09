cfdb23e00facab2a7e21a5acb044341497951412
pedrosimonetti@gmail.com <pedrosimonetti@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-02-09 19:01:13 +0000

    storing line numbers in each JSDOC.Token; fix missing semicolons;
    git-svn-id: http://fbug.googlecode.com/svn@9290 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- jsdoc/jsdoc-toolkit-2.4.0/app/lib/JSDOC/Token.js
+++ jsdoc/jsdoc-toolkit-2.4.0/app/lib/JSDOC/Token.js
@@ -3,16 +3,19 @@ if (typeof JSDOC == "undefined") JSDOC = {};
 /**
 	@constructor
 */
-JSDOC.Token = function(data, type, name) {
+JSDOC.Token = function(data, type, name, stream) {
 	this.data = data;
 	this.type = type;
 	this.name = name;
-}
+	
+	// TODO: xxxpedro
+	this.lineNumber = stream ? stream.lineNumber : -1;
+};
 
 JSDOC.Token.prototype.toString = function() { 
 	return "<"+this.type+" name=\""+this.name+"\">"+this.data+"</"+this.type+">";
-}
+};
 
 JSDOC.Token.prototype.is = function(what) {
 	return this.name === what || this.type === what;
-}
+};
