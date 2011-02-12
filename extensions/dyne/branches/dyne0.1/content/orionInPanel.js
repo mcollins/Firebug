ce0962fafe102bf0b035866de61e189f8eb4f159
johnjbarton@johnjbarton.com <johnjbarton@johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-02-12 05:51:50 +0000

    [dyne] in panel comes up, too tall, no ruler.
    git-svn-id: http://fbug.googlecode.com/svn@9334 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- extensions/dyne/branches/dyne0.1/content/orionInPanel.js
+++ extensions/dyne/branches/dyne0.1/content/orionInPanel.js
@@ -8,29 +8,27 @@
         - use the given stylesheet  */
 try
 {
+
     window.orion = {};
     orion.parent = document.getElementsByClassName('orionEditor')[0];
-
-    orion.editor = new eclipse.Editor({
-        parent: orion.parent,
-        readonly: false,
-        stylesheet: "http://download.eclipse.org/e4/orion/js/org.eclipse.orion.client.editor/editor.css"
-    });
-
-    // use javascript styler for now, there is no html/xml syntax highlighting yet
-    orion.styler = new eclipse.TextStyler(orion.editor, "js");
-    // add a ruler with line numbers to the left side
-   // orion.lines = new eclipse.LineNumberRuler("left", {styleClass: "ruler_lines"}, {styleClass: "ruler_lines_odd"}, {styleClass: "ruler_lines_even"});
-
-  //  orion.editor.addRuler(orion.lines);
-
-
     orion.parent.addEventListener('orionEdit', function updateText(event)
     {
-        var text = orion.editText;  // set by firebug
-        window.dump("orion text "+text+"\n");
         try
         {
+            orion.editor = new eclipse.Editor({
+                parent: orion.parent,
+                readonly: false,
+                stylesheet: "http://download.eclipse.org/e4/orion/js/org.eclipse.orion.client.editor/editor.css"
+            });
+
+            // use javascript styler for now, there is no html/xml syntax highlighting yet
+            orion.styler = new eclipse.TextStyler(orion.editor, "js");
+            // add a ruler with line numbers to the left side
+           // orion.lines = new eclipse.LineNumberRuler("left", {styleClass: "ruler_lines"}, {styleClass: "ruler_lines_odd"}, {styleClass: "ruler_lines_even"});
+
+          //  orion.editor.addRuler(orion.lines);
+
+            var text = orion.editText;  // set by firebug
             orion.editor.setText(text);
             //fix the height of the containing div
             orion.parent.style.height = (orion.editor.getLineHeight() * (orion.editor.getModel().getLineCount() + 1)) + 2 + 'px';
@@ -43,8 +41,6 @@ try
             window.FBTrace("orion.editor.setText ERROR "+exc, exc);
         }
     }, true);
-
-    window.dump("orionEditor "+orion.parent+"\n");
 }
 catch (exc)
 {
