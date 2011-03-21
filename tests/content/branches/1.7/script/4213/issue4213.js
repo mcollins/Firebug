25feef3569a008d5159f02607e1ef734e9e716f7
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-03-21 15:33:40 +0000

    [fbtest] yet more tracing for test script/issue4213
    git-svn-id: http://fbug.googlecode.com/svn@9731 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/branches/1.7/script/4213/issue4213.js
+++ tests/content/branches/1.7/script/4213/issue4213.js
@@ -19,7 +19,13 @@ function runTest()
                     stepInto(function hitNextBreak()
                     {
                         verifyLocation("issue4213-1.js");
-                        FBTest.testDone("issue4213.DONE");
+
+                        //xxxHonza: for debugging purposes, test bot fails on the
+                        // previous verifyLocation, but I can't repro that. 
+                        setTimeout(function() {
+                            verifyLocation("II. issue4213-1.js");
+                            FBTest.testDone("issue4213.DONE");
+                        }, 400);
                     });
                 });
 
