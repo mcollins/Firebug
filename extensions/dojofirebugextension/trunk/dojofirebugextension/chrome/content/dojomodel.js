34b3a1026cf6091ff2296ec7e1eaf554684fc333
patricioreyna@gmail.com <patricioreyna@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-03-30 17:13:11 +0000

    [dojo] lots of work in dojomodel's Maps to allow using injected hashcodes for comparisons as a workaround to == not working with our tracked objects in FF 4.0
    git-svn-id: http://fbug.googlecode.com/svn@9882 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- extensions/dojofirebugextension/trunk/dojofirebugextension/chrome/content/dojomodel.js
+++ extensions/dojofirebugextension/trunk/dojofirebugextension/chrome/content/dojomodel.js
@@ -21,6 +21,30 @@ var DojoModel = FBL.ns(function() { with (FBL) {
 				 	((fn.target) && (fn.target.internalClass == "dojoext-added-code")));
 	 };
 
+	var _isEnumerable = function(/*Object*/obj) {
+	     var t = typeof(obj);
+	     if (_isNumber(obj) || t == "boolean" || (t == "string" || t instanceof String)) {                
+	     	return true;
+	     }
+	     return false;
+	};
+	
+	var _isNumber = function(/*object*/obj) {     
+		return typeof(obj) == "number";                
+	};
+	
+	/*
+	 * FIXME xxxpreyna HACK required due to == returning false as of FF 4
+	 * (in FF 3.6 we can safely use == to compare Objects) 
+	 */
+	var areEqual = this.areEqual = function(obj1, obj2, usingHashcodes) {
+		if(!usingHashcodes || (_isEnumerable(obj1) || _isEnumerable(obj2))) {
+			return obj1 === obj2;
+		} else {
+			return HashCodeBasedDictionary.prototype.areEqual(obj1, obj2);
+		}		
+	};
+	
  /* ****************************************************************************
   * ****************************************************************************
   * ****************************************************************************
@@ -203,6 +227,9 @@ var DojoModel = FBL.ns(function() { with (FBL) {
 	 */
 	 var HashCodeBasedDictionary = function() {
 		 
+		//this is to avoid issues when inserting objects in more than one HashCodeBasedDictionary
+		this.keyIdPropertyName = this.keyIdPropertyNamePrefix + HashCodeBasedDictionary.prototype.instanceCount++;
+
 		// Next keys.
 		this._nextKey = 1;
 		
@@ -217,15 +244,23 @@ var DojoModel = FBL.ns(function() { with (FBL) {
 
 	 };
 	 HashCodeBasedDictionary.prototype = extend(Map.prototype,{
-			// Key id property name
-			keyIdPropertyName : 'x_dojoExtHashCode',
+		 	// classs variable
+		 	instanceCount: 0,		 
+			// Key id property name, class variable
+			keyIdPropertyNamePrefix: 'x_dojoExtHashCode_',
 		 
 			// Associates the specified value with the specified key in this map
-			put: function (key, value){
+			put: function (key, value) {
+				if(_isEnumerable(key)) {
+					if(FBTrace.DBG_DOJO) {
+						FBTrace.sysout("DOJO ERROR: tried to use HashCodeDictionary with a primitive type as key");
+					}
+					throw new Error("DOJO InvalidType: key is invalidType for HashCodeDictionary", key);
+				}
+		 		
 				var realKey = key[this.keyIdPropertyName];
-				if (!realKey || realKey == -1) {
-					realKey = (this._deprecatedKeys.length > 0) ? this._deprecatedKeys.pop() : this._nextKey++;					
-					this._injectKey(key, realKey);
+				if (!realKey || realKey == -1) {					
+					realKey = this._generateAndInjectKey(key);
 				}
 				this._keys[realKey] = key;
 				this._values[realKey] = value;
@@ -233,20 +268,36 @@ var DojoModel = FBL.ns(function() { with (FBL) {
 			},
 
 			// Returns the value to which this map maps the specified key
-			get: function (key){
+			get: function (key) {
+				
+				if(_isEnumerable(key)) {
+					if(FBTrace.DBG_DOJO) {
+						FBTrace.sysout("DOJO ERROR: tried to use HashCodeDictionary with a primitive type as key");
+					}
+					throw new Error("DOJO InvalidType: key is invalidType for HashCodeDictionary", key);
+				}
+				
 				var realKey = (key) ? key[this.keyIdPropertyName] : null;
 				return (realKey && realKey != -1) ? this._values[realKey] : null;
 			},
 
 			// Removes the mapping for this key from this map if it is present
-			remove: function (key){
+			remove: function (key) {
+				
+				if(_isEnumerable(key)) {
+					if(FBTrace.DBG_DOJO) {
+						FBTrace.sysout("DOJO ERROR: tried to use HashCodeDictionary with a primitive type as key");
+					}
+					throw new Error("DOJO InvalidType: key is invalidType for HashCodeDictionary", key);
+				}
+
+				
 				var realKey = key[this.keyIdPropertyName];
 				if (realKey && realKey != -1) {
 					this._keys[realKey] = null;
 					this._values[realKey] = null;
-					this._deprecatedKeys.push(realKey);
+					this._releaseInjectedKey(key);
 				}
-				delete key[this.keyIdPropertyName];
 			},
 			
 			// Keys getter.
@@ -274,21 +325,75 @@ var DojoModel = FBL.ns(function() { with (FBL) {
 				this._values = null;
 			},
 			
-			_injectKey: function(/*Object*/obj, /*int*/ hashcode) {
+			
+			_releaseInjectedKey: function(/*object*/key) {
+				var realKey = key[this.keyIdPropertyName];
+				this._deprecatedKeys.push(realKey);
+				delete key[this.keyIdPropertyName];
+			},
+			
+			/*int*/_generateAndInjectKey: function(/*Object*/obj) {
 				/*
 				 * since Javascript 1.8.5 (FF 4) we can create propeties in objects
 				 * that are not visible in for..in loops in client web pages.
 				 * https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Object/defineProperty 
 				 */
-				
-				if(Object.defineProperty) {
-					Object.defineProperty(obj, this.keyIdPropertyName, 
-							{value: hashcode, writable : false, enumerable : false, configurable : true});
-
-				} else {
-					//traditional way..
-					obj[this.keyIdPropertyName] = hashcode;
+				if(_isEnumerable(obj)) {
+					if(FBTrace.DBG_DOJO) {
+						FBTrace.sysout("DOJO ERROR: HashCodeDictionary tried to inject key field in primitive type");
+					}
+					throw new Error("DOJO InvalidType: obj is invalidType for injecting keys", obj);
 				}
+
+				var hashcode = (this._deprecatedKeys.length > 0) ? this._deprecatedKeys.pop() : this._nextKey++;
+				
+				try {
+					if(Object.defineProperty) {
+						Object.defineProperty(obj, this.keyIdPropertyName, 
+								{value: hashcode, writable : false, enumerable : false, configurable : true});
+	
+					} else {
+						//traditional way..
+						obj[this.keyIdPropertyName] = hashcode;
+					}
+					
+					return hashcode;
+					
+				} catch (exc) {
+					if(FBTrace.DBG_DOJO) {
+						FBTrace.sysout("DOJO ERROR while injecting key in hashcode based item", exc);
+					}
+					throw exc;
+				}
+			},
+			
+			areEqual: function(obj1, obj2) {
+				/*
+				 * FIXME xxxpreyna HACK required due to == returning false as of FF 4
+				 * (in FF 3.6 we can safely use == to compare Objects) 
+				 */
+				
+				if((obj1 && !obj2) || (!obj1 && obj2)) {
+					return false;
+				}
+
+				if((obj1 === null && obj2 === undefined) || (obj1 === undefined && obj2 === null)) {
+					return false;
+				}
+
+				var max = HashCodeBasedDictionary.prototype.instanceCount;
+				var hashcode1, hashcode2;
+				for (var i = 0; i < max; i++) {
+					hashcode1 = obj1[this.keyIdPropertyNamePrefix + i];
+					hashcode2 = obj2[this.keyIdPropertyNamePrefix + i];
+					if(hashcode1 != hashcode2) {
+						return false;
+					} else if (hashcode1 && hashcode2 && (hashcode1 == hashcode2)) {
+						return true;
+					}					
+				}
+				//the objs don't have hashcodes, let's compare them by ===
+				return obj1 === obj2;
 			}
 			
 	 });
@@ -348,7 +453,66 @@ var DojoModel = FBL.ns(function() { with (FBL) {
 				this._map = null;
 			} 
 	 });
+	 
+	 
+	 /**
+	  * @class ComposedDictionary 
+	  */
+	 var ComposedDictionary = function() {
+		// The map.
+		this._hashcodeKeyedValues = new HashCodeBasedDictionary();
+		this._primitiveKeyedValues = new StringMap();
+	 };
+	 ComposedDictionary.prototype = extend(Map.prototype, 
+	 {
+		 _hashcodeKeyedValues: null,
+		 _primitiveKeyedValues: null,
+		 
+		// Associates the specified value with the specified key in this map
+	 	put: function (key, value) {
+			if(_isEnumerable(key)) {
+				this._primitiveKeyedValues.put(key, value);
+			} else {
+				this._hashcodeKeyedValues.put(key, value);
+			}
+			return this;
+		},
 
+		// Returns the value to which this map maps the specified key
+		get: function (key){
+			if(_isEnumerable(key)) {
+				return this._primitiveKeyedValues.get(key);
+			} else {
+				return this._hashcodeKeyedValues.get(key);
+			}
+		},
+
+		// Removes the mapping for this key from this map if it is present
+		remove: function (key){
+			if(_isEnumerable(key)) {
+				this._primitiveKeyedValues.remove(key);
+			} else {
+				this._hashcodeKeyedValues.remove(key);
+			}
+		},
+		
+		// Keys getter.
+		/*array*/getKeys: function(){
+			return this._hashcodeKeyedValues.getKeys().concat(this._primitiveKeyedValues.getKeys());
+		},
+		
+		// Values getter.
+		/*array*/getValues: function(){
+			return this._hashcodeKeyedValues.getValues().concat(this._primitiveKeyedValues.getValues());
+		},
+		
+		// Destructor
+		destroy: function(){
+			this._hashcodeKeyedValues.destroy();
+			this._primitiveKeyedValues.destroy();
+		} 
+	 });
+	 
 	// ***************************************************************
 	 /**
 	 * @class EventListenerSupport
@@ -505,14 +669,14 @@ var DojoModel = FBL.ns(function() { with (FBL) {
 	  * @class Connection API
 	  */
 	 var ConnectionsAPI = this.ConnectionsAPI = function(/*boolean*/ useHashCodeBasedDictionary){
-		 // Connections dictionary.
-		 this._connections = (useHashCodeBasedDictionary) ? new HashCodeBasedDictionary() : /*new ArrayMap();*/ new Dictionary();
+		 // Connections dictionary (keys will be "dojo connection's source objects").
+		 this._connections = (useHashCodeBasedDictionary) ? new ComposedDictionary() /*new HashCodeBasedDictionary()*/ : /*new ArrayMap();*/ new Dictionary();
 		 
 		 // Array of connections.
 		 this._connectionsArray = [];
 		 
-		 // Disconnections dictionary.
-		 this._disconnections = (useHashCodeBasedDictionary) ? new HashCodeBasedDictionary() : /*new ArrayMap();*/ new Dictionary();
+		 // Disconnections dictionary. (keys will be "dojo handles")
+		 this._disconnections = (useHashCodeBasedDictionary) ? new ComposedDictionary() /*new HashCodeBasedDictionary()*/ : /*new ArrayMap();*/ new Dictionary();
 		 
 		 // Subscriptions
 		 this._subscriptions = new StringMap();		 
@@ -574,7 +738,7 @@ var DojoModel = FBL.ns(function() { with (FBL) {
 		  * This function return (and create if it does not exist for the key) the objectInfo
 		  * for the object passed as parameter.		  
 		  */ 
-		 _getAndCreateIfRequiredObjectInfo: function(obj) {
+		 /*ObjectInfo*/_getAndCreateIfRequiredObjectInfo: function(obj) {
 			    var objectInfo = this._connections.get(obj);
 			    if (!objectInfo){
 			 	   objectInfo = new ObjectInfo(obj);
@@ -674,7 +838,7 @@ var DojoModel = FBL.ns(function() { with (FBL) {
 		 /**
 		  * Return an object that contain the connections for the parameter object.
 		  */
-		 /*Connection*/getConnection: function(/*Object: conn's source or context object*/obj) {
+		 /*ObjectInfo*/getConnection: function(/*Object: conn's source or context object*/obj) {
 			 return this._connections.get(obj);
 		 },
 		 
@@ -688,7 +852,7 @@ var DojoModel = FBL.ns(function() { with (FBL) {
 		 /**
 		  * Return an array with the objects with connections.
 		  */
-		 /*array*/getObjectsWithConnections: function() {
+		 /*array<Object>*/getObjectsWithConnections: function() {
 			 return this._connections.getKeys();
 		 },
 		 
@@ -715,7 +879,7 @@ var DojoModel = FBL.ns(function() { with (FBL) {
 		 /**
 		  * Return the subscriptions map.
 		  */
-		 /*Object(Map)*/ getSubscriptions: function() {
+		 /*StringMap*/ getSubscriptions: function() {
 			 return this._subscriptions;
 		 },
 		 
@@ -919,7 +1083,7 @@ var DojoModel = FBL.ns(function() { with (FBL) {
 		this._incomingConnectionsCount = 0;
 		
 		// Outgoing connections (Dictionary<(String|Function),Connection>).
-		this._outgoingConnections = new Dictionary(); //must be Dictionary
+		this._outgoingConnections = new ComposedDictionary(); //new Dictionary(); //must be Dictionary
 		this._outgoingConnectionsCount = 0;
 	 };
 	 ConnectionsTracker.prototype = 
@@ -1220,9 +1384,14 @@ var DojoModel = FBL.ns(function() { with (FBL) {
     		return connectionArray;
     	};
     };
+    
+    
     this.ConnectionArraySorter.OBJ = 0;
     this.ConnectionArraySorter.EVENT = 1;
     this.ConnectionArraySorter.CONTEXT = 2;
     this.ConnectionArraySorter.METHOD = 3;
-	 
+
+/***********************************************************************************************************************/
+
+DojoExtension.DojoModel = DojoModel; 
 }});
