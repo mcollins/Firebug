aaec9b3f94a3473a84d5e2e774da7006f2e72172
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-04-11 12:20:19 +0000

    [fbtest] New test for dynamic script evaluation.
    git-svn-id: http://fbug.googlecode.com/svn@10005 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/branches/1.8/script/dynamicScriptEval/test1.js
@@ -0,0 +1,36 @@
+function runTest()
+{
+    FBTest.sysout("dynScriptLoad.START");
+    FBTest.openNewTab(basePath + "script/dynamicScriptEval/test1.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableNetPanel(function(win)
+        {
+            FBTest.openFirebug();
+            FBTest.enableAllPanels();
+            FBTest.selectPanel("console");
+
+            // Reload window to activate debugger.
+            FBTest.reload(function(win)
+            {
+                var config = {tagName: "div", classes: "logRow logRow-log", counter: 2};
+                FBTest.waitForDisplayedElement("console", config, function(row)
+                {
+                    var panel = FBTest.getPanel("console");
+                    var panelNode = panel.panelNode;
+
+                    var rows = panelNode.querySelectorAll(".logRow.logRow-log");
+                    FBTest.compare(2, rows.length, "There must be two logs");
+
+                    FBTest.compare("quack\n", rows[0].textContent, "The first log must say: quack");
+                    FBTest.compare("meow!!\n", rows[1].textContent, "The second log must say: meow!!");
+
+                    FBTest.testDone("console.log.DONE");
+                });
+
+                FBTest.clearConsole();
+                FBTest.click(win.document.getElementById("testButton"));
+            });
+        });
+    });
+}
