f499d3d45066b624e6a866e138de0e92b92789e8
patricioreyna@gmail.com <patricioreyna@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-04-13 22:56:53 +0000

    [tests] mozilla based testcase for issue with nativeWrappers and the == returning
    git-svn-id: http://fbug.googlecode.com/svn@10051 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/mozilla/mozilla_nativewrappers/content/mozilla_nativewrappers.js
@@ -0,0 +1,144 @@
+/* See license.txt for terms of usage */
+
+(function() {
+
+// ************************************************************************************************
+// Constants
+
+var Ci = Components.interfaces;
+var Cc = Components.classes;
+
+var observerService = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
+
+// ************************************************************************************************
+
+var Tracker = function () {
+	
+	this.trackedObjects = {};
+	
+	this.track = function(name, obj) {
+		this.trackedObjects[name] = obj;
+		return obj;
+	};
+	
+	this.getTracked = function(name) {
+		return this.trackedObjects[name];
+	};
+};
+
+var TrackerObserver =
+{
+	
+	initialize: function() {
+		//register as observer to be notified when a new page is created
+		observerService.addObserver(this, "content-document-global-created", false);
+		
+	    var appcontent = document.getElementById("appcontent");   // browser
+		if(appcontent) {
+			//also wants to be notified when a page finished loading...
+			appcontent.addEventListener("DOMContentLoaded", this.onPageLoad, true);
+		}
+
+		sysout("TrackerObserver activated");
+    },
+
+    shutdown: function() {
+
+	    var appcontent = document.getElementById("appcontent");   // browser
+		if(appcontent) {
+			appcontent.removeEventListener("DOMContentLoaded", this.onPageLoad, true);
+		}
+
+		observerService.removeObserver(this, "content-document-global-created", false);
+        sysout("TrackerObserver deactivated");
+    },
+
+	
+	//executed after a DOMContentLoaded happened in client context
+	onPageLoad: function(aEvent) {
+	
+		var doc = aEvent.originalTarget.wrappedJSObject; // doc is document that triggered "onload" event
+
+		sysout("onPageLoad: " + doc.location.href);
+	
+		if(doc.location.href.search("mozilla_nativewrappers") == -1)
+		  return;
+
+		var retrievedFn = doc.tracker.getTracked("fn");
+		var unwrappedOriginalFn = (doc.originalFn.wrappedJSObject) ? doc.originalFn.wrappedJSObject : doc.originalFn;
+		
+		sysout("comparison of fn is: " + (doc.originalFn == retrievedFn));		
+		sysout("comparison of fn.wrappedJSObject (if exists): " + (unwrappedOriginalFn == retrievedFn.wrappedJSObject));
+		sysout("comparison of fn.toSource() is: " + (doc.originalFn.toSource() == retrievedFn.toSource()));
+		  
+		 //if XPCNativeWrapper.unwrap is available...
+		if((typeof(XPCNativeWrapper) != "undefined") && XPCNativeWrapper.unwrap) {
+			var unwrappedFromPage = XPCNativeWrapper.unwrap(doc.originalFn);
+			var unwrappedRetrievedFn = XPCNativeWrapper.unwrap(retrievedFn);
+			sysout("comparison of XPCNativeWrapper.unwrap(fn) is: " + (unwrappedFromPage == unwrappedRetrievedFn));
+		}
+		  
+		// add event listener for page unload 
+		//aEvent.originalTarget.defaultView.addEventListener("unload", this.onPageUnload, true);
+	},
+
+	onPageUnload: function(aEvent) {
+		// do something
+	},
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+    observe: function(subject, topic, data)
+    {
+        if ((topic == "content-document-global-created") && (subject instanceof Ci.nsIDOMWindow)) { 
+			
+			var win = subject;
+            var docPage = win.wrappedJSObject.document;			
+
+			//INJECT Tracker in page 
+			var tracker = new Tracker();			
+			docPage.tracker = tracker;
+		
+			//also inject public functions in page to interact with the Tracker
+			var trackFn = function(name, obj) {
+				return tracker.track(name, obj);
+			};
+			_addMozillaExecutionGrants(trackFn);
+			docPage.track = trackFn;
+			
+			var getFn = function(name) {
+				return tracker.getTracked(name);
+			};			
+			_addMozillaExecutionGrants(getFn);
+			docPage.getTracked = getFn;
+		}
+    }
+};
+
+		
+//required as of FF 4
+function _addMozillaExecutionGrants(fn) {
+	if(!fn.__exposedProps__) {
+		fn.__exposedProps__ = {};
+	}		
+	fn.__exposedProps__.apply = "r";
+	fn.__exposedProps__.call = "r";
+}
+
+// ************************************************************************************************
+// Logging
+
+function sysout(msg)
+{
+    Components.utils.reportError(msg);
+    dump(msg + "\n");
+}
+
+// ************************************************************************************************
+// Registration
+
+window.addEventListener("load", function() { TrackerObserver.initialize(); }, false);
+window.addEventListener("unload", function() { TrackerObserver.shutdown(); }, false);
+
+// ************************************************************************************************
+})();
