53d4f3cc0255efa709532c5ce3bcd8b9141aecf9
honza@allpeers.com <honza@allpeers.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-04-21 16:46:12 +0000

    [tests] test files for issue 4341
    git-svn-id: http://fbug.googlecode.com/svn@10147 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/issues/4341/js.js
@@ -0,0 +1,26 @@
+var imc = new Class({
+	request : function () {
+		var jsonRequest = new Request.JSON({
+			noCache: true,
+			url: 'http://compojoom.com/firebug/firebug.php',
+			onSuccess: function(answer){
+
+				  if(answer.success == true) {
+					
+  					console.log('there are still products, let us match agian');
+  
+  					jsonRequest.send();
+  
+  				} else {
+  					console.log('Matchin complete');
+  
+  				}
+		    },
+		    onFailure: function(xhr) {
+          console.log(xht);
+        }
+    
+    });
+		jsonRequest.send();
+	}
+});
