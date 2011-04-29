9328723318ac4459941290e93bb5c3aa03209ae3
johnjbarton@johnjbarton.com <johnjbarton@johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-04-29 22:31:09 +0000

    [tests] more info in messages and more tests
    git-svn-id: http://fbug.googlecode.com/svn@10239 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/branches/1.8/loader/instanceof/main.js
+++ tests/content/branches/1.8/loader/instanceof/main.js
@@ -16,6 +16,8 @@ function runTest()
 
     require(["reps.js"], function(Reps)
     {
+        Reps.innerTest(FW, FBTest, window);
+
         var obj = new MyObject("hello");
 
         // obj is instance of MyObject
@@ -23,30 +25,32 @@ function runTest()
             "The object is instance of MyObject");
 
         FBTest.ok((Reps.instanceOf(obj, MyObject)),
-            "The object is instance of MyObject");
+            "The object is Reps.instanceOf MyObject");
 
         FBTest.ok((Reps.XW_instanceOf(FW.FBL, obj, MyObject)),
-            "The object is instance of MyObject");
+            "The object is Reps.XW_instanceOf MyObject");
 
         // obj is not instance of Window
         FBTest.ok(!(obj instanceof window.Window),
-            "The object is not an instance of Window");
+            "The object is not an instanceof window.Window");
 
         FBTest.ok(!(Reps.instanceOf(obj, window.Window)),
-            "The object is not an instance of Window");
+            "The object "+obj+" is not Reps.instanceOf window.Window "+window.Window);
+        if (Reps.instanceOf(obj, window.Window))
+            FBTest.progress("Failed "+Reps.instanceOf.toSource());
 
         FBTest.ok(!(Reps.XW_instanceOf(FW.FBL, obj, window.Window)),
-            "The object is not an instance of Window");
+            "The object is not an Reps.XW_instanceOf Window");
 
         // window is instance of Window
         FBTest.ok((window instanceof window.Window),
-            "The window is an instance of Window");
+            "The window is an instanceof window.Window");
 
         FBTest.ok((Reps.instanceOf(window, window.Window)),
-            "The window is an instance of Window");
+            "The window is Reps.instanceOf window.Window");
 
         FBTest.ok((Reps.XW_instanceOf(FW.FBL, window, window.Window)),
-            "The window is an instance of Window");
+            "The window is an Reps.XW_instanceOf window.Window");
 
         FBTest.testDone("instanceof.DONE");
     });
