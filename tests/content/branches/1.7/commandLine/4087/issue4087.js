7b0921204a6f44aecff5e643091dfc176346c555
sebastianzartner@gmx.de <sebastianzartner@gmx.de@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-05-04 08:48:10 +0000

    [fbtest] Automated test case for issue 4087 (Contents of Command Line and Command Editor do not persist on page reload) http://code.google.com/p/fbug/issues/detail?id=4087
    git-svn-id: http://fbug.googlecode.com/svn@10300 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/branches/1.7/commandLine/4087/issue4087.js
@@ -0,0 +1,37 @@
+function runTest()
+{
+    FBTest.sysout("issue4087.START");
+    FBTest.openNewTab(basePath + "commandLine/4087/issue4087.htm", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableConsolePanel(function(win)
+        {
+            var panel = FW.FirebugChrome.selectPanel("console");
+            var doc = FW.FirebugChrome.window.document;
+            var cmdLine = doc.getElementById("fbCommandLine");
+
+            // Test Command Line
+            FBTestFirebug.setPref("largeCommandLine", false);
+            cmdLine.value = ''; //xxxsz: Clear Command Line before typing the command - should probably be done inside typeCommand()
+            FBTest.typeCommand("var test = 'Hello';");
+  
+            FBTest.synthesizeKey("VK_F5", win);
+
+            FBTest.compare("var test = 'Hello';", cmdLine.value,
+                "Content of Command Line must be: var test = 'Hello';");
+
+            // Test Command Editor
+            FBTestFirebug.setPref("largeCommandLine", true);
+            cmdLine = doc.getElementById("fbLargeCommandLine");
+            cmdLine.value = '';
+            FBTest.typeCommand("var test = 'World';", true);
+  
+            FBTest.synthesizeKey("VK_F5", win);
+
+            FBTest.compare("var test = 'World';", cmdLine.value,
+            "Content of Command Editor must be: var test = 'World';");
+
+            FBTest.testDone("issue4087.DONE");
+        });
+    });
+}
