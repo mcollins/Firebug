596049cad298ecdc702da3bdec51ac8103d866e2
sebastianzartner@gmx.de <sebastianzartner@gmx.de@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-05-04 12:21:55 +0000

    [fbtest] Added test for issue 3709
    git-svn-id: http://fbug.googlecode.com/svn@10308 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/branches/1.8/commandLine/3709/issue3709.js
@@ -0,0 +1,25 @@
+function runTest()
+{
+    FBTest.sysout("issue3709.START");
+    FBTest.openNewTab(basePath + "commandLine/3709/issue3709.htm", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.enableConsolePanel(function(win)
+        {
+            var panel = FW.FirebugChrome.selectPanel("console");
+            var doc = FW.FirebugChrome.window.document;
+            var cmdLine = doc.getElementById("fbCommandLine");
+
+            // Test Command Line
+            cmdLine.value = ''; //xxxsz: Clear Command Line before typing the command - should probably be done inside typeCommand()
+            FBTest.typeCommand("document.getElementById()");
+            FBTest.synthesizeKey("VK_LEFT", win);
+            FBTest.typeCommand("ab");
+
+            FBTest.compare("document.getElementById(ab)", cmdLine.value,
+                "Content of Command Line must be: document.getElementById(ab)");
+
+            FBTest.testDone("issue3709.DONE");
+        });
+    });
+}
