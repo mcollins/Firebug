d3122ccce93792421e29ad38a1784bf31e4b5726
johnjbarton@johnjbarton.com <johnjbarton@johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-05-18 23:46:57 +0000

    [swarm] set the pref for no updates in user.js
    git-svn-id: http://fbug.googlecode.com/svn@10528 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- extensions/swarm/branches/swarm1.7/swarms/user.js
+++ extensions/swarm/branches/swarm1.7/swarms/user.js
@@ -4,3 +4,5 @@ user_pref("browser.shell.checkDefaultBrowser", false);
 user_pref("xpinstall.whitelist.add", "getfirebug.com");
 user_pref("extensions.fbtest.enableTestLogger", true);
 user_pref("extensions.logging.enabled", true);  // debug info for swarm installs.
+// We don't want to change the versions of extn we are testing
+user_pref("extensions.update.enabled", false);
