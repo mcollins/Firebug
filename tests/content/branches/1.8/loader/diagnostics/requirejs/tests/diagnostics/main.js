588c1f1b45a46605ec2f8d8a64785a729cb46602
johnjbarton@johnjbarton.com <johnjbarton@johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-05-22 17:46:02 +0000

    [tests] add tests for require.js user error diagnostics (partial)
    git-svn-id: http://fbug.googlecode.com/svn@10584 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/branches/1.8/loader/diagnostics/requirejs/tests/diagnostics/main.js
@@ -0,0 +1,114 @@
+function exceptionToString(exc)
+{
+    return exc.toString() +" "+(exc.fileName || exc.sourceName) + "@" + exc.lineNumber;
+}
+function runTest()
+{
+    FBTest.progress("diagnotics test start, using baseLocalPath " + baseLocalPath);
+
+    // ----------------------------------------------------------------------------------------------------
+    FBTest.progress("Null baseURL test");
+
+    var config = {
+        context: "testRequireJS" + Math.random(),  // to give each test its own loader,
+        baseUrl: null,  // << For first test
+        onDebug: function(msg)
+        {
+            FBTest.progress("onDebug: " + msg);
+        },
+        onError: function(msg)
+        {
+            FBTest.progress("onError: " + msg);
+            throw msg;
+        },
+        onTrace: function(msg)
+        {
+            FBTest.progress("onTrace:" + msg);
+        },
+        debug: true,
+    };
+
+    var require = FBTest.getRequire();
+
+    var onErrorMessage = "";
+    require.onError = function(msg) {
+        onErrorMessage = ""+msg;
+        throw msg;
+    }
+
+    try
+    {
+        require(config, ["baseURLIsNull"], function(baseURLIsNull)
+                {
+                    // We never get here.
+                });
+    }
+    catch(exc)
+    {
+        FBTest.sysout("baseURLIsNull ERROR "+exceptionToString(exc) );
+    }
+    finally
+    {
+        FBTest.compare("No baseUrl, needed for URL: baseURLIsNull.js", onErrorMessage, "Test Null baseUrl message");
+    }
+
+
+    delete config.baseUrl;
+
+    // ----------------------------------------------------------------------------------------------------
+    FBTest.progress("Bad baseURL test");
+
+    var badBase = "NoEndingSlash";
+    config.baseUrl = badBase + "loader/diagnostics/";
+
+    onErrorMessage = null;
+
+    try
+    {
+        require(config, ["badBaseURL"], function(Bad)
+        {
+            FBTest.sysout("baseBaseURL callback called");
+        });
+    }
+    catch(exc)
+    {
+        FBTest.sysout("baseBaseURL ERROR "+exceptionToString(exc) );
+    }
+    finally
+    {
+        FBTest.compare("Bad baseUrl, needed for URL: baseURLIsNull.js", onErrorMessage, "Test bad baseURL ");
+    }
+
+    return;
+    // ----------------------------------------------------------------------------------------------------
+    FBTest.progress("Syntax Error test");
+
+
+    if (baseLocalPath)
+    {
+        config.baseUrl = baseLocalPath + "/loader/diagnostics/";
+    }
+
+    onErrorMessage = null;
+    require.onError = function(msg) {
+        onErrorMessage = msg;
+    }
+
+    try
+    {
+        require(config, ["syntaxErrorInsideDefine"], function(syntaxError)
+        {
+            var message = A.getMessage();
+            FBTest.compare("Hello World!", message, "The message from modules must match.");
+            FBTest.testDone("dependencies.DONE");
+        });
+    }
+    catch(exc)
+    {
+        FBTest.sysout("syntaxErrorInsideDefine ERROR "+exceptionToString(exc) );
+    }
+    finally
+    {
+        FBTest.compare("Some syntax error", onErrorMessage, "Test syntax error in define()");
+    }
+}
