7c31dc5ce7adbe69eb7116791f4113a0368fca94
sebastianzartner@gmx.de <sebastianzartner@gmx.de@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-05-23 06:43:59 +0000

    Fixed translated UI comparison
    git-svn-id: http://fbug.googlecode.com/svn@10586 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/branches/1.8/console/api/trace.js
+++ tests/content/branches/1.8/console/api/trace.js
@@ -12,13 +12,17 @@ function runTest()
                 var stackFrames = row.getElementsByClassName("objectBox-stackFrame");
                 FBTest.compare(2, stackFrames.length, "There must be 2 stack frames.");
 
-                var reStack1 = /onExecuteTest\(\)\s*trace.html\s*\(line 34\)/;
+                var reStack1 = new RegExp("onExecuteTest\\(\\)\\s*" +
+                    FW.FBL.$STRF("Line", ["trace.html", 34]).replace(/([\\"'\(\)])/g, "\\$1"));
                 FBTest.compare(reStack1, stackFrames[0].textContent, "The first stack frame text must match.");
 
                 FBTest.progress("Found stack frame "+stackFrames[1].textContent);
-                var reStack2 = /onclick\(Object\s*{\s*name=\"event\"}\)1\s*\(line\s*2\)/;  // before R5281
-                var reStack2 = /onclick\(event=click\s*clientX=0,\s*clientY=0\)1\s*\(line\s*2\)/; // after R7281
-                var reStack3 = /onclick\(event=click\s*clientX=0,\s*clientY=0\)onclick\s*\(line\s*2\)/; // after R10542
+                var reStack2 = new RegExp("onclick\\(Object\\s*{\\s*name=\\\"event\\\"}\\)1\\s*" +
+                    FW.FBL.$STRF("Line", ["", 2]).replace(/([\\"'\(\)])/g, "\\$1"));  // before R5281
+                var reStack2 = new RegExp("onclick\\(event=click\\s*clientX=0,\\s*clientY=0\\)1\\s*" +
+                    FW.FBL.$STRF("Line", ["", 2]).replace(/([\\"'\(\)])/g, "\\$1")); // after R7281
+                var reStack3 = new RegExp("onclick\\(event=click\\s*clientX=0,\\s*clientY=0\\)" +
+                    FW.FBL.$STRF("Line", ["onclick", 2]).replace(/([\\"'\(\)])/g, "\\$1")); // after R10542
                 FBTest.compare(reStack3, stackFrames[1].textContent, "The second stack frame text must match.");
 
                 FBTest.testDone("console.trace.DONE");
