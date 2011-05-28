f75366f9d45a0882ab7116b2dfc9369463772701
johnjbarton@johnjbarton.com <johnjbarton@johnjbarton.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-05-28 00:40:00 +0000

    [dyne] jsedit test for tdd
    git-svn-id: http://fbug.googlecode.com/svn@10667 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ extensions/dyne/branches/dyne0.1/content/tests/jsedit/jsedit.js
@@ -0,0 +1,57 @@
+var userPartFileName = "jsEditThisJs.js";
+var fileName = basePath + "jsedit/" + userPartFileName;
+var lineNo = 2;
+
+function runTest()
+{
+    FBTest.sysout("jsedit runTest starts basePath: "+basePath);
+
+    FBTestFirebug.openNewTab(basePath + "jsedit/jsedit.html", function()
+    {
+        // Open Firebug UI, enable Script panel, reload and start first test.
+        FBTestFirebug.openFirebug();
+        FBTestFirebug.clearCache();
+        FBTestFirebug.selectPanel("script");
+        FBTestFirebug.enableScriptPanel(function callbackOnReload(testWindow) {
+            win = testWindow;
+            selectFile();
+            editLine();
+        });
+    });
+}
+function editLine() {
+    var chrome = FW.Firebug.chrome;
+
+    FBTestFirebug.selectSourceLine(fileName, lineNo, "js", chrome, function(row)
+    {
+        FBTest.progress("Found line "+lineNo+" "+row);
+        FBTest.sysout("row ", row);
+        var sourceRowText = row.getElementsByClassName('sourceRowText').item(0);
+        var sourceText = sourceRowText.textContent;
+        var changeMe = sourceText.split("\"")[1];
+        FBTest.compare("change me", changeMe, "The original source must be found");
+        var index = sourceText.indexOf(changeMe);
+        FBTest.progress(" change me is at "+index);
+        applyEdit(sourceRowText, index, changeMe.length, "ok then");
+
+    });
+}
+
+function applyEdit(element, index, length, toText) {
+    var text = element.innerHTML;
+    FBTest.progress("innerHTML" +text);
+    var result = text.substring(0, index) + toText + text.substring(index+length);
+    element.innerHTML = result;
+    FBTest.progress("result" +result);
+}
+
+function selectFile()
+{
+    FBTest.progress("selectFile");
+
+    // Select proper JS file.
+    var panel = FBTestFirebug.getSelectedPanel();
+    FBTest.progress("panel.location "+panel.location.getURL());
+    var found = FBTestFirebug.selectPanelLocationByName(panel, userPartFileName);
+    FBTest.compare(found, true, "The "+fileName+" should be found");
+}
