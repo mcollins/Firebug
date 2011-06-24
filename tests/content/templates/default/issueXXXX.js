01d2d406a8e8c313d6dd40e1402dd10930923304
sebastianzartner@gmx.de <sebastianzartner@gmx.de@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-06-24 21:27:46 +0000

    [tests] General template for automated tests
    git-svn-id: http://fbug.googlecode.com/svn@11108 e969d3be-0e28-0410-a27f-dd5c76401a8b

---
+++ tests/content/templates/default/issueXXXX.js
@@ -0,0 +1,24 @@
+/*
+    1. Replace "XXXX" by number of your issue
+    2. Replace "mainPanel" by main panel name
+    3. Replace "doSomething" by first step after selecting the main panel
+    4. Add the functionality and tests as described in the steps of the HTML file
+    5. Remove all template comments
+*/
+function runTest()
+{
+    FBTest.sysout("issueXXXX.START");
+
+    FBTest.openNewTab(basePath + "net/XXXX/issueXXXX.html", function(win)
+    {
+        FBTest.openFirebug();
+        FBTest.selectPanel("mainPanel");
+
+        FBTest.doSomething(function(win)
+        {
+            // Tests must be placed here
+
+            FBTest.testDone("issueXXXX.DONE");
+        });
+    });
+}
