7ca563d843d2e32c8b594509506029958b4f4222
sebastianzartner@gmx.de <sebastianzartner@gmx.de@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-06-28 07:59:14 +0000

    [tests] Added step for replacing "category"
    git-svn-id: http://fbug.googlecode.com/svn@11140 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- tests/content/templates/default/issueXXXX.js
+++ tests/content/templates/default/issueXXXX.js
@@ -1,15 +1,16 @@
 /*
     1. Replace "XXXX" by number of your issue
-    2. Replace "mainPanel" by main panel name
-    3. Replace "doSomething" by first step after selecting the main panel
-    4. Add the functionality and tests as described in the steps of the HTML file
-    5. Remove all template comments
+    2. Replace "category" by the folder name you put your test case to
+    3. Replace "mainPanel" by main panel name
+    4. Replace "doSomething" by first step after selecting the main panel
+    5. Add the functionality and tests as described in the steps of the HTML file
+    6. Remove all template comments
 */
 function runTest()
 {
     FBTest.sysout("issueXXXX.START");
 
-    FBTest.openNewTab(basePath + "net/XXXX/issueXXXX.html", function(win)
+    FBTest.openNewTab(basePath + "category/XXXX/issueXXXX.html", function(win)
     {
         FBTest.openFirebug();
         FBTest.selectPanel("mainPanel");
